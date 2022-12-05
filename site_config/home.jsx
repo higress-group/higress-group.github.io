a035c1418b6ed11a56f01aceed700ea2bf1b0daa
澄潭 <zty98751@alibaba-inc.com>
2022-12-05 21:16:03 +0800

    Update home.jsx

--- site_config/home.jsx
+++ site_config/home.jsx
@@ -163,7 +163,7 @@ export default {
           title: 'community',
           list: [
               'https://img.alicdn.com/imgextra/i4/O1CN01lP4p411R49V3tQpFH_!!6000000002057-0-tps-1062-1410.jpg' ,
-              'https://img.alicdn.com/imgextra/i1/O1CN01UfZyN31zNZE4P1cES_!!6000000006702-0-tps-1008-1240.jpg' ,
+              'https://img.alicdn.com/imgextra/i3/O1CN01RkvQ3J1WMXUBDJ2y6_!!6000000002774-2-tps-1056-1371.png' ,
           ],
       },
     },
