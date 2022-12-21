ec80f88c29263577a569d28ee4e27f658b93d333
kwtboom <117439825+kwtboom@users.noreply.github.com>
2022-12-21 10:11:05 +0800

    feat：首页调整及增加微服务全景图 (#51)

--- src/pages/home/index.jsx
+++ src/pages/home/index.jsx
@@ -10,6 +10,7 @@ import Item from './featureItem';
 import Bone from '../../components/bone';
 import homeConfig from '../../../site_config/home.jsx';
 import './index.scss';
+// import MseArc from '../../../umd/mse-arc-ui.min.js'
 
 class Home extends Language {
 
@@ -64,8 +65,8 @@ class Home extends Language {
         <section className="top-section">
           <div className="top-body">
             <div className="vertical-middle">
-              <div className="product-name">
-                <h2>{dataSource.brand.brandName}</h2>
+              <div className="product-name" >
+                <img src={getLink(dataSource.brand.brandLogoUrl)} />
               </div>
               <p className="product-desc">
                 {dataSource.brand.briefIntroduction}
@@ -157,6 +158,12 @@ class Home extends Language {
             <div className="img-wrapper">
               <img src={getLink(dataSource.introduction.img)} />
             </div>
+          </div>
+        </section>
+        <section className="msemap-section">
+          <div className="msemap-container">
+            <h3>微服务全景图</h3>
+            <div id="mse-arc-container"></div>
           </div>
         </section>
         <section className="feature-section">
