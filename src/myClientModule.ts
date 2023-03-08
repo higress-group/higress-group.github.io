6dfc4633af8bc5f61426b96ffcc5d70d107fab60
老俊 <782803291@qq.com>
2023-03-08 20:57:29 +0800

    fix: fix default lang

--- src/myClientModule.ts
+++ src/myClientModule.ts
@@ -1,10 +1,9 @@
 import type { ClientModule } from '@docusaurus/types';
 
 const module: ClientModule = {
-  onRouteUpdate({ location, previousLocation }) {
-    console.log('------', location);
+  onRouteUpdate({ location }) {
     if (!(location.pathname.startsWith('/zh-cn') || location.pathname.startsWith('/en-us'))) {
-      window.location.href = 'zh-cn';
+      window.location.href = `/zh-cn${location.pathname}`;
     }
   },
 };
