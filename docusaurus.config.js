175d42c74ca7ac8d6a857438e5fdb5fc1c5da856
zxj <782803291@qq.com>
2023-03-16 10:06:13 +0800

    fix: fix images in en-us (#100)

--- docusaurus.config.js
+++ docusaurus.config.js
@@ -41,7 +41,7 @@ const config = {
     },
   },
   scripts: [
-    { src: '//g.alicdn.com/mamba/assets/0.0.14/mse-arc-ui.min.js' },
+    { src: '//g.alicdn.com/mamba/assets/0.0.15/mse-arc-ui.min.js' },
     {
       src: '//g.alicdn.com/alilog/mlog/aplus_v2.js',
       id: 'beacon-aplus',
@@ -58,7 +58,7 @@ const config = {
   ],
   stylesheets: [
     {
-      href: '//g.alicdn.com/mamba/assets/0.0.14/mse-arc-ui.min.css',
+      href: '//g.alicdn.com/mamba/assets/0.0.15/mse-arc-ui.min.css',
     },
   ],
   plugins: ['docusaurus-plugin-sass'],
