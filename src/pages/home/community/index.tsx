203e2719fe16506be76266e79f696d34f889410e
Kent Dong <ch3cho@qq.com>
2023-04-18 01:23:14 +0800

    fix: Update the title of community section of homepage (#126)

--- src/pages/home/community/index.tsx
+++ src/pages/home/community/index.tsx
@@ -4,7 +4,7 @@ import './index.scss';
 import BrowserOnly from '@docusaurus/BrowserOnly';
 
 const data = {
-  title: translate({ id: 'homepage.introTitle', message: '搭把手，共建Higress' }),
+  title: translate({ id: 'homepage.introTitle', message: '参与 Higress 社区' }),
   list: [
     'https://img.alicdn.com/imgextra/i4/O1CN01lP4p411R49V3tQpFH_!!6000000002057-0-tps-1062-1410.jpg',
     'https://img.alicdn.com/imgextra/i3/O1CN01RkvQ3J1WMXUBDJ2y6_!!6000000002774-2-tps-1056-1371.png',
