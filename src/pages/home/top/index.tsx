a406ee5b91c7e6a0d702fff74a756ba1ca04d2e7
澄潭 <zty98751@alibaba-inc.com>
2023-05-22 21:01:07 +0800

    Update index.tsx

--- src/pages/home/top/index.tsx
+++ src/pages/home/top/index.tsx
@@ -28,8 +28,8 @@ const topData = {
     },
   ],
   versionNote: {
-    text: 'Release Note of 1.0.0-rc',
-    link: 'https://github.com/alibaba/higress/releases/tag/v1.0.0-rc',
+    text: 'Release Note of 1.0.0',
+    link: 'https://github.com/alibaba/higress/releases/tag/v1.0.0',
   },
   releaseDate: 'Released on Apr 10, 2023',
 };
