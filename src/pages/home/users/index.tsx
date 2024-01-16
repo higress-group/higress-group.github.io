70bd081d04a6f37036d922761875b8d740fc9b6f
澄潭 <zty98751@alibaba-inc.com>
2024-01-16 16:13:28 +0800

    Update index.tsx

--- src/pages/home/users/index.tsx
+++ src/pages/home/users/index.tsx
@@ -15,6 +15,7 @@ const data = {
       <Translate id="homepage.userDesc2">上提供信息来获取 Higress 开源落地支持</Translate>
     </span>
   ),
+  
   list: [
     'https://img.alicdn.com/imgextra/i1/O1CN01TleQq128FAP8POtL5_!!6000000007902-2-tps-241-42.png',
     'https://img.alicdn.com/imgextra/i4/O1CN01IOIk0s1bWgJ05f4HF_!!6000000003473-2-tps-1200-699.png',
@@ -22,6 +23,8 @@ const data = {
     'https://img.alicdn.com/imgextra/i3/O1CN01pH2InM1iuDzpDxxox_!!6000000004472-0-tps-228-82.jpg',
     'https://img.alicdn.com/imgextra/i3/O1CN013OMegK1xQCGX3CQNQ_!!6000000006437-0-tps-178-88.jpg',
     'https://img.alicdn.com/imgextra/i1/O1CN01vrxF2I1rfs7rihLdy_!!6000000005659-0-tps-310-160.jpg',
+    'https://img.alicdn.com/imgextra/i2/O1CN01wbAkCU1JitvJcJilZ_!!6000000001063-2-tps-2560-680.png',
+    'https://img.alicdn.com/imgextra/i2/O1CN01xY9jNx27ygN9k8iYO_!!6000000007866-0-tps-1650-891.jpg',
     'https://img.alicdn.com/imgextra/i2/O1CN018rBpj61JL5L7kbcTe_!!6000000001011-2-tps-1058-450.png',
     'https://img.alicdn.com/imgextra/i1/O1CN01638eOX1ZcW9bTIID4_!!6000000003215-2-tps-700-394.png',
     'https://img.alicdn.com/imgextra/i2/O1CN01T8c4by29tlErCcjHw_!!6000000008126-2-tps-170-170.png',
@@ -35,7 +38,6 @@ const data = {
     'https://img.alicdn.com/imgextra/i2/O1CN01jZVSJ21XmTdX3M1M3_!!6000000002966-0-tps-462-109.jpg',
     'https://img.alicdn.com/imgextra/i4/O1CN01XNXCIJ1SqzSgWfMyr_!!6000000002299-0-tps-200-200.jpg',
     'https://img.alicdn.com/imgextra/i2/O1CN01zGLOSN1H6liYr71AH_!!6000000000709-0-tps-160-160.jpg',
-    'https://img.alicdn.com/imgextra/i2/O1CN01wbAkCU1JitvJcJilZ_!!6000000001063-2-tps-2560-680.png',
     'https://img.alicdn.com/imgextra/i2/O1CN016jVoUz1TrHKLtarsx_!!6000000002435-0-tps-204-192.jpg',
     'https://img.alicdn.com/imgextra/i2/O1CN01vaysCW1LZOwOMRk0e_!!6000000001313-2-tps-200-200.png',
     'https://img.alicdn.com/imgextra/i1/O1CN01xxepgv1WGaRShnpyV_!!6000000002761-0-tps-325-260.jpg',
