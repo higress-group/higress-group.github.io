384229010316c5269385eac3980b727cd2b1d288
澄潭 <zty98751@alibaba-inc.com>
2024-03-05 09:36:04 +0800

    Update index.tsx

--- src/pages/home/top/index.tsx
+++ src/pages/home/top/index.tsx
@@ -28,10 +28,10 @@ const topData = {
     },
   ],
   versionNote: {
-    text: 'Release Note of 1.3.0',
-    link: 'https://github.com/alibaba/higress/releases/tag/v1.3.0',
+    text: 'Release Note of 1.3.5',
+    link: 'https://github.com/alibaba/higress/releases/tag/v1.3.5',
   },
-  releaseDate: 'Released on Nov 3, 2023',
+  releaseDate: 'Released on Mar 4, 2024',
 };
 
 const Top = ({ language }: { language?: string }) => {
