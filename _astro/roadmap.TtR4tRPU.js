c13a6e7ec7d427321553e637d397ff57dc71c17b
johnlanni <johnlanni@users.noreply.github.com>
2024-07-30 08:00:05 +0000

    Automated deployment: Tue Jul 30 08:00:05 UTC 2024 c07dc4539779f8539143cc84973675a229876e7c

---
+++ _astro/roadmap.TtR4tRPU.js
@@ -0,0 +1,14 @@
+const a="latest/zh-cn/overview/roadmap.md",e="docs",t="latest/zh-cn/overview/roadmap",o=`
+# \u7248\u672C\u8BA1\u5212
+
+| \u7248\u672C\u53F7  | \u7248\u672C\u6838\u5FC3\u76EE\u6807 | \u9884\u8BA1\u53D1\u5E03\u65F6\u95F4 |
+| ------- | -----------  | -----------  |
+| v0.6.0  | K8s \u4E0B\u9ED1\u5C4F\u64CD\u4F5C(kubectl)\u8FBE\u5230**GA\u72B6\u6001**\uFF1B\u53D1\u5E03\u63A7\u5236\u53F0\u9884\u89C8\u7248\uFF0C\u63D0\u9AD8\u5B89\u88C5\u90E8\u7F72\u7684\u6613\u7528\u6027   | 2023-01   |
+| v0.7.0  | \u63A7\u5236\u53F0\u53D1\u5E03\u6B63\u5F0F\u7248\u672C\uFF0C\u96C6\u6210\u5F00\u7BB1\u5373\u7528\u7684\u53EF\u89C2\u6D4B   | 2023-02   |
+| v1.0.0  | \u6574\u4F53\u8FBE\u5230**GA\u72B6\u6001**   | 2023-05   |
+| v1.1.0  | \u589E\u5F3A\u63A7\u5236\u53F0\u529F\u80FD\u4E2D\u5FAE\u670D\u52A1\u7F51\u5173\u76F8\u5173\u80FD\u529B\uFF1B\u652F\u6301\u975E K8s \u4E0B\u5B89\u88C5\u90E8\u7F72   | 2023-07   |
+| v1.2.0  | \u652F\u6301\u4F5C\u4E3A Knative \u7684\u7F51\u7EDC\u5C42\u4F7F\u7528\uFF1B\u652F\u6301 ARM \u67B6\u6784\u90E8\u7F72\uFF1B\u652F\u6301\u5BF9\u63A5 Consul   | 2023-08   |
+| v1.3.0  | \u5B8C\u6574\u652F\u6301 GatewayAPI\uFF1B\u652F\u6301 Operator \u6A21\u5F0F\u90E8\u7F72   | 2023-11   |
+| v1.4.0  | WASM \u652F\u6301 redis \u8C03\u7528\uFF1B\u7B80\u5316 HTTPS \u8BC1\u4E66\u7BA1\u7406  | 2024-05   |
+| v2.0.0  | Istio/Envoy \u5185\u6838\u7248\u672C\u5347\u7EA7\uFF0C\u66F4\u4E30\u5BCC\u7684 API \u7F51\u5173\u80FD\u529B  | 2024-07   |
+`,s={title:"Roadmap",description:"Higress Roadmap.",editUrl:!0,head:[],template:"doc",sidebar:{hidden:!1,attrs:{}},pagefind:!0,keywords:["Higress","roadmap"]},r={type:"content",filePath:"/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/zh-cn/overview/roadmap.md",rawData:void 0};export{r as _internal,o as body,e as collection,s as data,a as id,t as slug};
