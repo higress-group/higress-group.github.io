0d4c0f250c875e28a03d7503f64f025b4bbbb7b2
Hazel0928 <Hazel0928@users.noreply.github.com>
2024-08-01 03:56:41 +0000

    Automated deployment: Thu Aug  1 03:56:41 UTC 2024 44226fd6e4f68e29162e357d03ccdee5e2a17b62

---
+++ _astro/ip-restriction.B6FQ4PGU.js
@@ -0,0 +1,91 @@
+import{c,__tla as p}from"./astro-component.DeDyO-JF.js";import{r as l,m as y,u,__tla as v}from"./constant.CPvuqMat.js";import{__tla as _}from"./astro/assets-service.B3vdYoe2.js";let e,r,s,d,n,o,a,E=Promise.all([(()=>{try{return p}catch{}})(),(()=>{try{return v}catch{}})(),(()=>{try{return _}catch{}})()]).then(async()=>{let t;t=`<h2 id="\u529F\u80FD\u8BF4\u660E">\u529F\u80FD\u8BF4\u660E</h2>
+<p><code dir="auto">ip-restriction</code> \u63D2\u4EF6\u53EF\u4EE5\u901A\u8FC7\u5C06 IP \u5730\u5740\u5217\u5165\u767D\u540D\u5355\u6216\u9ED1\u540D\u5355\u6765\u9650\u5236\u5BF9\u670D\u52A1\u6216\u8DEF\u7531\u7684\u8BBF\u95EE\u3002\u652F\u6301\u5BF9\u5355\u4E2A IP \u5730\u5740\u3001\u591A\u4E2A IP \u5730\u5740\u548C\u7C7B\u4F3C 10.10.10.0/24 \u7684 CIDR \u8303\u56F4\u7684\u9650\u5236\u3002</p>
+<h2 id="\u914D\u7F6E\u8BF4\u660E">\u914D\u7F6E\u8BF4\u660E</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<table><thead><tr><th>\u914D\u7F6E\u9879</th><th>\u7C7B\u578B</th><th>\u5FC5\u586B</th><th>\u9ED8\u8BA4\u503C</th><th>\u8BF4\u660E</th></tr></thead><tbody><tr><td>ip_source_type</td><td>string</td><td>\u5426</td><td>origin-source</td><td>\u53EF\u9009\u503C\uFF1A1. \u5BF9\u7AEF socket ip\uFF1A<code dir="auto">origin-source</code>; 2. \u901A\u8FC7 header \u83B7\u53D6\uFF1A<code dir="auto">header</code></td></tr><tr><td>ip_header_name</td><td>string</td><td>\u5426</td><td>x-forwarded-for</td><td>\u5F53 <code dir="auto">ip_source_type</code> \u4E3A <code dir="auto">header</code> \u65F6\uFF0C\u6307\u5B9A\u81EA\u5B9A\u4E49 IP \u6765\u6E90\u5934</td></tr><tr><td>allow</td><td>array</td><td>\u5426</td><td>[]</td><td>\u767D\u540D\u5355\u5217\u8868</td></tr><tr><td>deny</td><td>array</td><td>\u5426</td><td>[]</td><td>\u9ED1\u540D\u5355\u5217\u8868</td></tr><tr><td>status</td><td>int</td><td>\u5426</td><td>403</td><td>\u62D2\u7EDD\u8BBF\u95EE\u65F6\u7684 HTTP \u72B6\u6001\u7801</td></tr><tr><td>message</td><td>string</td><td>\u5426</td><td>Your IP address is blocked.</td><td>\u62D2\u7EDD\u8BBF\u95EE\u65F6\u7684\u8FD4\u56DE\u4FE1\u606F</td></tr></tbody></table>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#85E89D">ip_source_type</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">origin-source</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">allow</span><span style="--0:#E1E4E8">:</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#79B8FF">10.0.0.1</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#9ECBFF">192.168.0.0/16</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="ip_source_type: origin-source\x7Fallow:\x7F  - 10.0.0.1\x7F  - 192.168.0.0/16"><div></div></button></div></figure></div>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#85E89D">ip_source_type</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">header</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">ip_header_name</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">x-real-iP</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">deny</span><span style="--0:#E1E4E8">:</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#79B8FF">10.0.0.1</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#9ECBFF">192.169.0.0/16</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="ip_source_type: header\x7Fip_header_name: x-real-iP\x7Fdeny:\x7F  - 10.0.0.1\x7F  - 192.169.0.0/16"><div></div></button></div></figure></div>`,d={title:"IP \u9650\u5236",keywords:["higress"],description:"IP \u9650\u5236\u63D2\u4EF6\u901A\u8FC7\u5C06 IP \u5730\u5740\u5217\u5165\u767D\u540D\u5355\u6216\u9ED1\u540D\u5355\u6765\u9650\u5236\u5BF9\u670D\u52A1\u6216\u8DEF\u7531\u7684\u8BBF\u95EE\u3002\u652F\u6301\u5BF9\u5355\u4E2A IP \u5730\u5740\u3001\u591A\u4E2A IP \u5730\u5740\u548C\u7C7B\u4F3C 10.10.10.0/24 \u7684 CIDR \u8303\u56F4\u7684\u9650\u5236\u3002"},s="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/zh-cn/plugins/traffic/ip-restriction.md",a=void 0,o=function(){return`
+## \u529F\u80FD\u8BF4\u660E
+
+\`ip-restriction\` \u63D2\u4EF6\u53EF\u4EE5\u901A\u8FC7\u5C06 IP \u5730\u5740\u5217\u5165\u767D\u540D\u5355\u6216\u9ED1\u540D\u5355\u6765\u9650\u5236\u5BF9\u670D\u52A1\u6216\u8DEF\u7531\u7684\u8BBF\u95EE\u3002\u652F\u6301\u5BF9\u5355\u4E2A IP \u5730\u5740\u3001\u591A\u4E2A IP \u5730\u5740\u548C\u7C7B\u4F3C 10.10.10.0/24 \u7684 CIDR \u8303\u56F4\u7684\u9650\u5236\u3002
+
+## \u914D\u7F6E\u8BF4\u660E
+
+| \u914D\u7F6E\u9879            | \u7C7B\u578B     | \u5FC5\u586B | \u9ED8\u8BA4\u503C                         | \u8BF4\u660E                                                            |
+|----------------|--------|----|-----------------------------|---------------------------------------------------------------|
+| ip_source_type | string | \u5426  | origin-source               | \u53EF\u9009\u503C\uFF1A1. \u5BF9\u7AEF socket ip\uFF1A\`origin-source\`; 2. \u901A\u8FC7 header \u83B7\u53D6\uFF1A\`header\` |
+| ip_header_name | string | \u5426  | x-forwarded-for             | \u5F53 \`ip_source_type\` \u4E3A \`header\` \u65F6\uFF0C\u6307\u5B9A\u81EA\u5B9A\u4E49 IP \u6765\u6E90\u5934                  |
+| allow          | array  | \u5426  | []                          | \u767D\u540D\u5355\u5217\u8868                                                         |
+| deny           | array  | \u5426  | []                          | \u9ED1\u540D\u5355\u5217\u8868                                                         |
+| status         | int    | \u5426  | 403                         | \u62D2\u7EDD\u8BBF\u95EE\u65F6\u7684 HTTP \u72B6\u6001\u7801                                               |
+| message        | string | \u5426  | Your IP address is blocked. | \u62D2\u7EDD\u8BBF\u95EE\u65F6\u7684\u8FD4\u56DE\u4FE1\u606F                                                    |
+
+
+\`\`\`yaml
+ip_source_type: origin-source
+allow:
+  - 10.0.0.1
+  - 192.168.0.0/16
+\`\`\`
+
+\`\`\`yaml
+ip_source_type: header
+ip_header_name: x-real-iP
+deny:
+  - 10.0.0.1
+  - 192.169.0.0/16   
+\`\`\`
+`},r=function(){return t},n=function(){return[{depth:2,slug:"\u529F\u80FD\u8BF4\u660E",text:"\u529F\u80FD\u8BF4\u660E"},{depth:2,slug:"\u914D\u7F6E\u8BF4\u660E",text:"\u914D\u7F6E\u8BF4\u660E"}]},e=c((h,g,f)=>{const{layout:m,...i}=d;return i.file=s,i.url=a,l`${y()}${u(t)}`})});export{e as Content,E as __tla,r as compiledContent,e as default,s as file,d as frontmatter,n as getHeadings,o as rawContent,a as url};
---
+++ _astro/prometheus.DgXoBS1G.js
@@ -0,0 +1,49 @@
+import{c as p,__tla as d}from"./astro-component.DeDyO-JF.js";import{r as h,m as c,u,__tla as g}from"./constant.CPvuqMat.js";import{__tla as m}from"./astro/assets-service.B3vdYoe2.js";let s,i,t,a,r,l,n,y=Promise.all([(()=>{try{return d}catch{}})(),(()=>{try{return g}catch{}})(),(()=>{try{return m}catch{}})()]).then(async()=>{let e;e=`<h2 id="use-the-higress-console-built-in-monitor-suite">Use the Higress Console Built-In Monitor Suite</h2>
+<p>Higress Console has a built-in monitor suite based on Prometheus + Grafana, which isn\u2019t installed by default. When installing Higress with Helm, you can enable it by adding the following argument to the command line: <code dir="auto">--set higress-console.o11y.enabled=true</code>.</p>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">helm</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">repo</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">add</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress.io</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">https://higress.io/helm-charts</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">helm</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">install</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-n</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress-system</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress.io/higress</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--create-namespace</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--render-subchart-notes</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--set</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress-console.o11y.enabled=</span><span style="--0:#79B8FF">true</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="helm repo add higress.io https://higress.io/helm-charts\x7Fhelm install higress -n higress-system higress.io/higress --create-namespace --render-subchart-notes --set higress-console.o11y.enabled=true"><div></div></button></div></figure></div>
+<p>Note: When installing to a standard K8s cluster (meaning without specifying <code dir="auto">global.local=true</code> or <code dir="auto">global.kind=true</code>), the PersistentVolumeClaim configured in the suite uses the access mode of <code dir="auto">ReadWriteMany</code> by default. If the target K8s cluster doesn\u2019t support this mode, you can add the following argument to the command line to change the access mode to <code dir="auto">ReadWriteOnce</code>: <code dir="auto">--set higress-console.pvc.rwxSupported=false</code>.</p>
+<div class="expressive-code"><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">helm</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">repo</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">add</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress.io</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">https://higress.io/helm-charts</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">helm</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">install</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-n</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress-system</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress.io/higress</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--create-namespace</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--render-subchart-notes</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--set</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress-console.o11y.enabled=</span><span style="--0:#79B8FF">true</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--set</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">higress-console.pvc.rwxSupported=</span><span style="--0:#79B8FF">false</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="helm repo add higress.io https://higress.io/helm-charts\x7Fhelm install higress -n higress-system higress.io/higress --create-namespace --render-subchart-notes --set higress-console.o11y.enabled=true --set higress-console.pvc.rwxSupported=false"><div></div></button></div></figure></div>
+<p>After installation, open Higress Console in your browser, and click \u201CDashboard\u201D on the left navigation area. The built-in dashboard will be displayed on the right.</p>
+<p><img src="/img/user/prometheus/en-us/builtin.png" alt="image" referrerpolicy="no-referrer"></p>
+<h2 id="use-a-standalone-monitor-suite">Use a Standalone Monitor Suite</h2>
+<p>Both the data and control planes of Higress expose a set of metrics via API. To monitor them, you just need to add probe configurations into Prometheus and configure a dashboard in Grafana.</p>
+<p>Before starting the configuration, please open Higress Console in your browser and switch to \u201CDashboard\u201D from the left navigation area. The external dashboard configuration page and related notes will be displayed on the right.</p>
+<p><img src="/img/user/prometheus/en-us/external-before.png" alt="image" referrerpolicy="no-referrer"></p>
+<p>Update the configurations of Prometheus and Grafana following the notes. Then put the Grafana dashboard into the textbox above and save. The page will reload automatically and your dashboard will be shown on the right.</p>
+<p><img src="/img/user/prometheus/en-us/external-after.png" alt="image" referrerpolicy="no-referrer"></p>
+<p>If you\u2019d like to update the dashboard URL, you can click the \u201CReconfigure\u201D button on the top left corner.</p>`,a={title:"Request Monitoring Based on Prometheus",keywords:["Higress","Prometheus"],description:"Request Monitoring Based on Prometheus",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/en-us/docusaurus-plugin-content-docs/current/user/prometheus.md"},t="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/en/user/prometheus.md",n=void 0,l=function(){return`
+# Request Monitoring Based on Prometheus
+
+## Use the Higress Console Built-In Monitor Suite
+
+Higress Console has a built-in monitor suite based on Prometheus + Grafana, which isn't installed by default. When installing Higress with Helm, you can enable it by adding the following argument to the command line: \`--set higress-console.o11y.enabled=true\`.
+
+\`\`\`bash
+helm repo add higress.io https://higress.io/helm-charts
+helm install higress -n higress-system higress.io/higress --create-namespace --render-subchart-notes --set higress-console.o11y.enabled=true
+\`\`\`
+
+Note: When installing to a standard K8s cluster (meaning without specifying \`global.local=true\` or \`global.kind=true\`), the PersistentVolumeClaim configured in the suite uses the access mode of \`ReadWriteMany\` by default. If the target K8s cluster doesn't support this mode, you can add the following argument to the command line to change the access mode to \`ReadWriteOnce\`: \`--set higress-console.pvc.rwxSupported=false\`.
+
+\`\`\`bash
+helm repo add higress.io https://higress.io/helm-charts
+helm install higress -n higress-system higress.io/higress --create-namespace --render-subchart-notes --set higress-console.o11y.enabled=true --set higress-console.pvc.rwxSupported=false
+\`\`\`
+
+After installation, open Higress Console in your browser, and click "Dashboard" on the left navigation area. The built-in dashboard will be displayed on the right.
+
+![image](/img/user/prometheus/en-us/builtin.png)
+
+## Use a Standalone Monitor Suite
+
+Both the data and control planes of Higress expose a set of metrics via API. To monitor them, you just need to add probe configurations into Prometheus and configure a dashboard in Grafana.
+
+Before starting the configuration, please open Higress Console in your browser and switch to "Dashboard" from the left navigation area. The external dashboard configuration page and related notes will be displayed on the right.
+
+![image](/img/user/prometheus/en-us/external-before.png)
+
+Update the configurations of Prometheus and Grafana following the notes. Then put the Grafana dashboard into the textbox above and save. The page will reload automatically and your dashboard will be shown on the right.
+
+![image](/img/user/prometheus/en-us/external-after.png)
+
+If you'd like to update the dashboard URL, you can click the "Reconfigure" button on the top left corner.`},i=function(){return e},r=function(){return[{depth:2,slug:"use-the-higress-console-built-in-monitor-suite",text:"Use the Higress Console Built-In Monitor Suite"},{depth:2,slug:"use-a-standalone-monitor-suite",text:"Use a Standalone Monitor Suite"}]},s=p((E,f,b)=>{const{layout:F,...o}=a;return o.file=t,o.url=n,h`${c()}${u(e)}`})});export{s as Content,y as __tla,i as compiledContent,s as default,t as file,a as frontmatter,r as getHeadings,l as rawContent,n as url};
