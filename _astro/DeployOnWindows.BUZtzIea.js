8478021d7421a6517cb2eab68ff8e85123221869
Hazel0928 <Hazel0928@users.noreply.github.com>
2024-09-11 08:55:50 +0000

    Automated deployment: Wed Sep 11 08:55:49 UTC 2024 08759141f16438a1b94a2143789d6aa36625f9bb

---
+++ _astro/DeployOnWindows.BUZtzIea.js
@@ -0,0 +1,193 @@
+import{c,__tla as l}from"./astro-component.cI6d52vQ.js";import{r as d,m as g,u as h,__tla as w}from"./constant.Cb-7D0bG.js";import{__tla as u}from"./astro/assets-service.Da9pL5MS.js";let e,p,o,n,t,r,i,y=Promise.all([(()=>{try{return l}catch{}})(),(()=>{try{return w}catch{}})(),(()=>{try{return u}catch{}})()]).then(async()=>{let s;s=`<h2 id="\u524D\u7F6E\u51C6\u5907">\u524D\u7F6E\u51C6\u5907</h2>
+<h3 id="\u914D\u7F6E-wsl2">\u914D\u7F6E WSL2</h3>
+<p>\u8BE6\u60C5\u53C2\u770B\u6B65\u9AA41-5,\u987A\u4FBF\u5728\u5FAE\u8F6F\u5546\u5E97\u4E2D\u4E0B\u8F7DTerminal\u3002</p>
+<p><a href="https://learn.microsoft.com/zh-cn/windows/wsl/install-manual" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">WSL\u624B\u52A8\u5B89\u88C5\u6B65\u9AA4</a></p>
+<h3 id="\u4E0B\u8F7D-docker-desktop">\u4E0B\u8F7D Docker Desktop</h3>
+<ol>
+<li>
+<p><strong>\u8BBF\u95EE Docker Desktop \u5B98\u65B9\u4E0B\u8F7D\u9875\u9762</strong></p>
+<p>\u5728\u6D4F\u89C8\u5668\u4E2D\u6253\u5F00 <a href="https://www.docker.com/products/docker-desktop" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">Docker Desktop \u7684\u5B98\u65B9\u4E0B\u8F7D\u9875\u9762</a></p>
+</li>
+<li>
+<p><strong>\u4E0B\u8F7D Docker Desktop</strong></p>
+<p>\u5728\u4E0B\u8F7D\u9875\u9762\uFF0C\u627E\u5230\u9002\u7528\u4E8E Windows \u7684 Docker Desktop \u7248\u672C\uFF0C\u7136\u540E\u70B9\u51FB\u4E0B\u8F7D\u3002</p>
+</li>
+<li>
+<p><strong>\u8FD0\u884C\u5B89\u88C5\u7A0B\u5E8F</strong></p>
+<p>\u4E0B\u8F7D\u5B8C\u6210\u540E\uFF0C\u627E\u5230\u4E0B\u8F7D\u7684\u5B89\u88C5\u7A0B\u5E8F\uFF08\u901A\u5E38\u5728\u4F60\u7684 \u201C\u4E0B\u8F7D\u201D \u6587\u4EF6\u5939\u4E2D\uFF09\uFF0C\u7136\u540E\u53CC\u51FB\u8FD0\u884C\u3002</p>
+</li>
+</ol>
+<h3 id="\u5B89\u88C5-cygwin">\u5B89\u88C5 Cygwin</h3>
+<p><a href="http://www.cygwin.com/" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">cygwin\u5B98\u7F51</a></p>
+<p>\u9009\u62E9setup-x86_64.exe\uFF0C\u7B49\u5F85\u5B89\u88C5\u5B8C\u6210\u3002</p>
+<h4 id="\u9A8C\u8BC1-cygwin-\u5B89\u88C5\u662F\u5426\u6210\u529F">\u9A8C\u8BC1 Cygwin \u5B89\u88C5\u662F\u5426\u6210\u529F</h4>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">cygcheck</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-c</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">cygwin</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="cygcheck -c cygwin"><div></div></button></div></figure></div>
+<p><img src="/img/blog/windows/pic/7.png" alt="7.png" referrerpolicy="no-referrer"></p>
+<h4 id="\u4E3A-cygwin-\u914D\u7F6E\u73AF\u5883\u53D8\u91CF">\u4E3A Cygwin \u914D\u7F6E\u73AF\u5883\u53D8\u91CF</h4>
+<p><img src="/img/blog/windows/pic/8.png" alt="8.png" referrerpolicy="no-referrer"></p>
+<p><img src="/img/blog/windows/pic/9.png" alt="9.png" referrerpolicy="no-referrer"></p>
+<p><img src="/img/blog/windows/pic/10.png" alt="10.png" referrerpolicy="no-referrer"></p>
+<p><img src="/img/blog/windows/pic/11.png" alt="11.png" referrerpolicy="no-referrer"></p>
+<p>\u70B9\u51FB\u786E\u5B9A\u5373\u53EF\u6DFB\u52A0\u6210\u529F</p>
+<h2 id="\u5B89\u88C5-higrees">\u5B89\u88C5 Higrees</h2>
+<h3 id="\u51C6\u5907-nacos">\u51C6\u5907 Nacos</h3>
+<p><a href="https://nacos.io/zh-cn/docs/v2/quickstart/quick-start-docker.html" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">nacos\u5B98\u7F51\u624B\u518C</a></p>
+<p>\u6211\u4EEC\u8FD9\u91CC\u9009\u62E9nacos-docker\u7684\u6A21\u5F0F\u5B89\u88C5</p>
+<p><img src="/img/blog/windows/pic/A.png" alt="A.png" referrerpolicy="no-referrer"></p>
+<p>\u4E0B\u8F7D\u89E3\u538Bzip\u6587\u4EF6,\u8FDB\u5165 nacos-docker-master \u6587\u4EF6\u5939\u53F3\u952E\u9009\u62E9\u7EC8\u7AEF\u6253\u5F00\uFF0C\u6267\u884C\u547D\u4EE4\uFF0C\u6211\u4EEC\u8FD9\u91CC\u9009\u62E9\u5355\u673A\u6A21\u5F0F\u90E8\u7F72</p>
+<div class="expressive-code"><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">    </span></span><span style="--0:#E1E4E8">docker</span><span style="--0:#F97583">-</span><span style="--0:#E1E4E8">compose </span><span style="--0:#F97583">-f</span><span style="--0:#E1E4E8"> example</span><span style="--0:#F97583">/</span><span style="--0:#E1E4E8">standalone</span><span style="--0:#F97583">-</span><span style="--0:#E1E4E8">derby.yaml up</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="docker-compose -f example/standalone-derby.yaml up"><div></div></button></div></figure></div>
+<p>\u7B49\u5F85\u51FA\u73B0\u754C\u9762\uFF0C\u5373\u5B89\u88C5\u6210\u529F</p>
+<p><img src="/img/blog/windows/pic/B.png" alt="B.png" referrerpolicy="no-referrer"></p>
+<h3 id="\u4F7F\u7528-higrees-\u5BF9\u63A5-nacos">\u4F7F\u7528 Higrees \u5BF9\u63A5 Nacos</h3>
+<p><strong>\u5B89\u88C5\u547D\u4EE4\uFF1A\u4F7F\u7528\u72EC\u7ACB\u90E8\u7F72\u7684 Nacos</strong></p>
+<p>\u5F53\u8BBF\u95EEdocker\u5BB9\u5668\u4E92\u76F8\u8BBF\u95EE\u65F6\u5019\u672C\u5730\u56DE\u73AF\u5730\u5740\u5E76\u4E0D\u662F\u771F\u6B63\u7684\u5730\u5740\uFF0C\u6240\u4EE5\u9700\u8981\u5728cygwin\u4E2D\u6267\u884C\u83B7\u53D6\u672C\u5730\u7F51\u5361\u5730\u5740</p>
+<div class="expressive-code"><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">ipconfig</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="ipconfig"><div></div></button></div></figure></div>
+<p><img src="/img/blog/windows/pic/C.png" alt="C.png" referrerpolicy="no-referrer"></p>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#e1e4e8">curl -fsSL https://higress.io/standalone/get-higress.sh | bash -s -- -c nacos://192.168.0.1:8848 --nacos-username=nacos --nacos-password=nacos</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="curl -fsSL https://higress.io/standalone/get-higress.sh | bash -s -- -c nacos://192.168.0.1:8848 --nacos-username=nacos --nacos-password=nacos"><div></div></button></div></figure></div>
+<p>\u8BF7\u5C06 <code dir="auto">192.168.0.1</code> \u66FF\u6362\u4E3A Nacos \u670D\u52A1\u5668\u7684 IP\uFF08\u5982\u679C Nacos \u90E8\u7F72\u5728\u672C\u673A\uFF0C\u8BF7\u4E0D\u8981\u4F7F\u7528\u5982 <code dir="auto">localhost</code> \u6216 <code dir="auto">127.0.0.1</code> \u7684 Loopback \u5730\u5740\uFF09\uFF0C\u5E76\u6309\u9700\u8C03\u6574 <code dir="auto">--nacos-username</code> \u548C <code dir="auto">--nacos-password</code> \u7684\u53D6\u503C\u3002\u5982\u679C Nacos \u670D\u52A1\u672A\u5F00\u542F\u8BA4\u8BC1\u529F\u80FD\uFF0C\u5219\u53EF\u4EE5\u79FB\u9664\u8FD9\u4E24\u4E2A\u53C2\u6570\u3002</p>
+<p>\u5728\u8FD9\u91CC\u672A\u5F00\u542F\u6388\u6743\u670D\u52A1\uFF0C\u76F4\u63A5\u4F7F\u7528WLANIP\u66FF\u6362\u5BF9\u5E94\u7684IP</p>
+<div class="expressive-code"><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">curl</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-fsSL</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">https://higress.io/standalone/get-higress.sh</span><span style="--0:#E1E4E8"> </span><span style="--0:#F97583">|</span><span style="--0:#E1E4E8"> </span><span style="--0:#B392F0">bash</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-s</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">--</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-c</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">nacos://10.30.0.225:8848</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="curl -fsSL https://higress.io/standalone/get-higress.sh | bash -s -- -c nacos://10.30.0.225:8848"><div></div></button></div></figure></div>
+<p>\u8F93\u5165\u547D\u4EE4\u7B49\u5F85\u90E8\u7F72\uFF0C\u5373\u53EF\u770B\u5230\u751F\u6210\u7684\u7528\u6237\u540D\u4E0E\u5BC6\u7801</p>
+<p><img src="/img/blog/windows/pic/D.png" alt="D.png" referrerpolicy="no-referrer"></p>
+<h3 id="higress-\u63A7\u5236\u53F0\u914D\u7F6E">Higress \u63A7\u5236\u53F0\u914D\u7F6E</h3>
+<p><strong>\u8BBF\u95EE <code dir="auto">http://localhost:8080/</code>, \u767B\u5F55 Higress \u63A7\u5236\u53F0\u3002\u9996\u6B21\u8BBF\u95EE\u65F6\u9700\u8981\u5148\u8BBE\u7F6E\u7528\u6237\u540D\u5BC6\u7801</strong></p>
+<p><img src="/img/blog/windows/pic/13.png" alt="13.png" referrerpolicy="no-referrer"></p>
+<p><strong>\u70B9\u51FB\u5DE6\u4FA7\u201C\u670D\u52A1\u6765\u6E90\u201D\u5BFC\u822A\u680F\uFF0C\u7136\u540E\u70B9\u51FB\u9875\u9762\u53F3\u4FA7\u7684\u201C\u521B\u5EFA\u670D\u52A1\u6765\u6E90\u201D\u6309\u94AE\u3002\u6309\u7167\u4E0B\u56FE\u6240\u793A\u5185\u5BB9\u586B\u5199\u8868\u5355\u5E76\u70B9\u51FB\u201C\u786E\u5B9A\u201D\u6309\u94AE\u3002</strong></p>
+<p><img src="/img/blog/windows/pic/14.png" alt="14.png" referrerpolicy="no-referrer"></p>
+<p><strong>\u70B9\u51FB\u5DE6\u4FA7\u201C\u57DF\u540D\u7BA1\u7406\u201D\u5BFC\u822A\u680F\uFF0C\u7136\u540E\u70B9\u51FB\u9875\u9762\u53F3\u4FA7\u7684\u201C\u521B\u5EFA\u57DF\u540D\u201D\u6309\u94AE\u3002\u6309\u7167\u4E0B\u56FE\u6240\u793A\u5185\u5BB9\u586B\u5199\u8868\u5355\u5E76\u70B9\u51FB\u201C\u786E\u5B9A\u201D\u6309\u94AE\u3002</strong></p>
+<p><img src="/img/blog/windows/pic/15.png" alt="15.png" referrerpolicy="no-referrer"></p>
+<p><strong>\u70B9\u51FB\u5DE6\u4FA7\u201C\u8DEF\u7531\u7BA1\u7406\u201D\u5BFC\u822A\u680F\uFF0C\u7136\u540E\u70B9\u51FB\u9875\u9762\u53F3\u4FA7\u7684\u201C\u521B\u5EFA\u8DEF\u7531\u201D\u6309\u94AE\u3002\u6309\u7167\u4E0B\u56FE\u7247\u6240\u793A\u5185\u5BB9\u586B\u5199\u8868\u5355\u5E76\u70B9\u51FB\u201C\u786E\u5B9A\u201D\u6309\u94AE\u3002</strong></p>
+<p><img src="/img/blog/windows/pic/16.png" alt="16.png" referrerpolicy="no-referrer"></p>
+<h3 id="\u8BF7\u6C42\u9A8C\u8BC1">\u8BF7\u6C42\u9A8C\u8BC1</h3>
+<div class="expressive-code"><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#99A0A6"># should output a JSON object containing request data</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">curl</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">http://localhost/get?foo=bar</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-H</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">'host: foo.bar.com'</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="curl http://localhost/get?foo=bar -H &#x27;host: foo.bar.com&#x27;"><div></div></button></div></figure></div>
+<p><img src="/img/blog/windows/pic/E.png" alt="E.png" referrerpolicy="no-referrer"></p>
+<p>\u66F4\u591A\u8BE6\u60C5\u4E0E\u90E8\u7F72\u65B9\u6848\u53EF\u53C2\u8003 <a href="https://higress.cn/docs/latest/user/quickstart/" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">quick start</a></p>`,n={title:"Windows \u4E0B Higress \u90E8\u7F72\u5B9E\u8DF5",keywords:["higress"],description:"Windows\u90E8\u7F72Higress",author:"Fkbqf",date:"2023-07-28",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-blog/DeployOnWindows.md",category:"ecosystem"},o="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/blog/DeployOnWindows.md",i=void 0,r=function(){return`## \u524D\u7F6E\u51C6\u5907
+
+###  \u914D\u7F6E WSL2
+\u8BE6\u60C5\u53C2\u770B\u6B65\u9AA41-5,\u987A\u4FBF\u5728\u5FAE\u8F6F\u5546\u5E97\u4E2D\u4E0B\u8F7DTerminal\u3002
+
+[WSL\u624B\u52A8\u5B89\u88C5\u6B65\u9AA4](https://learn.microsoft.com/zh-cn/windows/wsl/install-manual)
+
+### \u4E0B\u8F7D Docker Desktop
+
+1. **\u8BBF\u95EE Docker Desktop \u5B98\u65B9\u4E0B\u8F7D\u9875\u9762**
+
+   \u5728\u6D4F\u89C8\u5668\u4E2D\u6253\u5F00 [Docker Desktop \u7684\u5B98\u65B9\u4E0B\u8F7D\u9875\u9762](https://www.docker.com/products/docker-desktop)
+
+2. **\u4E0B\u8F7D Docker Desktop**
+
+   \u5728\u4E0B\u8F7D\u9875\u9762\uFF0C\u627E\u5230\u9002\u7528\u4E8E Windows \u7684 Docker Desktop \u7248\u672C\uFF0C\u7136\u540E\u70B9\u51FB\u4E0B\u8F7D\u3002
+
+3. **\u8FD0\u884C\u5B89\u88C5\u7A0B\u5E8F**
+
+   \u4E0B\u8F7D\u5B8C\u6210\u540E\uFF0C\u627E\u5230\u4E0B\u8F7D\u7684\u5B89\u88C5\u7A0B\u5E8F\uFF08\u901A\u5E38\u5728\u4F60\u7684 "\u4E0B\u8F7D" \u6587\u4EF6\u5939\u4E2D\uFF09\uFF0C\u7136\u540E\u53CC\u51FB\u8FD0\u884C\u3002
+
+
+
+### \u5B89\u88C5 Cygwin
+
+[cygwin\u5B98\u7F51](http://www.cygwin.com/)
+
+
+\u9009\u62E9setup-x86_64.exe\uFF0C\u7B49\u5F85\u5B89\u88C5\u5B8C\u6210\u3002
+
+
+#### \u9A8C\u8BC1 Cygwin \u5B89\u88C5\u662F\u5426\u6210\u529F
+\`\`\`shell
+cygcheck -c cygwin
+\`\`\`
+
+![7.png](/img/blog/windows/pic/7.png)
+
+#### \u4E3A Cygwin \u914D\u7F6E\u73AF\u5883\u53D8\u91CF
+
+![8.png](/img/blog/windows/pic/8.png)
+
+
+![9.png](/img/blog/windows/pic/9.png)
+
+
+![10.png](/img/blog/windows/pic/10.png)
+
+
+![11.png](/img/blog/windows/pic/11.png)
+
+
+\u70B9\u51FB\u786E\u5B9A\u5373\u53EF\u6DFB\u52A0\u6210\u529F
+
+
+## \u5B89\u88C5 Higrees
+
+### \u51C6\u5907 Nacos 
+[nacos\u5B98\u7F51\u624B\u518C](https://nacos.io/zh-cn/docs/v2/quickstart/quick-start-docker.html)
+
+\u6211\u4EEC\u8FD9\u91CC\u9009\u62E9nacos-docker\u7684\u6A21\u5F0F\u5B89\u88C5
+
+![A.png](/img/blog/windows/pic/A.png)
+
+\u4E0B\u8F7D\u89E3\u538Bzip\u6587\u4EF6,\u8FDB\u5165 nacos-docker-master \u6587\u4EF6\u5939\u53F3\u952E\u9009\u62E9\u7EC8\u7AEF\u6253\u5F00\uFF0C\u6267\u884C\u547D\u4EE4\uFF0C\u6211\u4EEC\u8FD9\u91CC\u9009\u62E9\u5355\u673A\u6A21\u5F0F\u90E8\u7F72
+
+\`\`\`powershell
+    docker-compose -f example/standalone-derby.yaml up
+\`\`\`
+
+\u7B49\u5F85\u51FA\u73B0\u754C\u9762\uFF0C\u5373\u5B89\u88C5\u6210\u529F
+
+![B.png](/img/blog/windows/pic/B.png)
+
+### \u4F7F\u7528 Higrees \u5BF9\u63A5 Nacos
+**\u5B89\u88C5\u547D\u4EE4\uFF1A\u4F7F\u7528\u72EC\u7ACB\u90E8\u7F72\u7684 Nacos**
+
+\u5F53\u8BBF\u95EEdocker\u5BB9\u5668\u4E92\u76F8\u8BBF\u95EE\u65F6\u5019\u672C\u5730\u56DE\u73AF\u5730\u5740\u5E76\u4E0D\u662F\u771F\u6B63\u7684\u5730\u5740\uFF0C\u6240\u4EE5\u9700\u8981\u5728cygwin\u4E2D\u6267\u884C\u83B7\u53D6\u672C\u5730\u7F51\u5361\u5730\u5740
+
+\`\`\`shell
+ipconfig
+\`\`\`
+
+![C.png](/img/blog/windows/pic/C.png)
+
+
+\`\`\`
+curl -fsSL https://higress.io/standalone/get-higress.sh | bash -s -- -c nacos://192.168.0.1:8848 --nacos-username=nacos --nacos-password=nacos
+\`\`\`
+
+\u8BF7\u5C06 \`192.168.0.1\` \u66FF\u6362\u4E3A Nacos \u670D\u52A1\u5668\u7684 IP\uFF08\u5982\u679C Nacos \u90E8\u7F72\u5728\u672C\u673A\uFF0C\u8BF7\u4E0D\u8981\u4F7F\u7528\u5982 \`localhost\` \u6216 \`127.0.0.1\` \u7684 Loopback \u5730\u5740\uFF09\uFF0C\u5E76\u6309\u9700\u8C03\u6574 \`--nacos-username\` \u548C \`--nacos-password\` \u7684\u53D6\u503C\u3002\u5982\u679C Nacos \u670D\u52A1\u672A\u5F00\u542F\u8BA4\u8BC1\u529F\u80FD\uFF0C\u5219\u53EF\u4EE5\u79FB\u9664\u8FD9\u4E24\u4E2A\u53C2\u6570\u3002
+
+
+\u5728\u8FD9\u91CC\u672A\u5F00\u542F\u6388\u6743\u670D\u52A1\uFF0C\u76F4\u63A5\u4F7F\u7528WLANIP\u66FF\u6362\u5BF9\u5E94\u7684IP
+\`\`\`shell
+curl -fsSL https://higress.io/standalone/get-higress.sh | bash -s -- -c nacos://10.30.0.225:8848
+
+\`\`\`
+\u8F93\u5165\u547D\u4EE4\u7B49\u5F85\u90E8\u7F72\uFF0C\u5373\u53EF\u770B\u5230\u751F\u6210\u7684\u7528\u6237\u540D\u4E0E\u5BC6\u7801
+
+![D.png](/img/blog/windows/pic/D.png)
+
+### Higress \u63A7\u5236\u53F0\u914D\u7F6E
+
+**\u8BBF\u95EE \`http://localhost:8080/\`, \u767B\u5F55 Higress \u63A7\u5236\u53F0\u3002\u9996\u6B21\u8BBF\u95EE\u65F6\u9700\u8981\u5148\u8BBE\u7F6E\u7528\u6237\u540D\u5BC6\u7801**
+
+![13.png](/img/blog/windows/pic/13.png)
+
+**\u70B9\u51FB\u5DE6\u4FA7\u201C\u670D\u52A1\u6765\u6E90\u201D\u5BFC\u822A\u680F\uFF0C\u7136\u540E\u70B9\u51FB\u9875\u9762\u53F3\u4FA7\u7684\u201C\u521B\u5EFA\u670D\u52A1\u6765\u6E90\u201D\u6309\u94AE\u3002\u6309\u7167\u4E0B\u56FE\u6240\u793A\u5185\u5BB9\u586B\u5199\u8868\u5355\u5E76\u70B9\u51FB\u201C\u786E\u5B9A\u201D\u6309\u94AE\u3002**
+
+![14.png](/img/blog/windows/pic/14.png)
+
+**\u70B9\u51FB\u5DE6\u4FA7\u201C\u57DF\u540D\u7BA1\u7406\u201D\u5BFC\u822A\u680F\uFF0C\u7136\u540E\u70B9\u51FB\u9875\u9762\u53F3\u4FA7\u7684\u201C\u521B\u5EFA\u57DF\u540D\u201D\u6309\u94AE\u3002\u6309\u7167\u4E0B\u56FE\u6240\u793A\u5185\u5BB9\u586B\u5199\u8868\u5355\u5E76\u70B9\u51FB\u201C\u786E\u5B9A\u201D\u6309\u94AE\u3002**
+
+![15.png](/img/blog/windows/pic/15.png)
+
+**\u70B9\u51FB\u5DE6\u4FA7\u201C\u8DEF\u7531\u7BA1\u7406\u201D\u5BFC\u822A\u680F\uFF0C\u7136\u540E\u70B9\u51FB\u9875\u9762\u53F3\u4FA7\u7684\u201C\u521B\u5EFA\u8DEF\u7531\u201D\u6309\u94AE\u3002\u6309\u7167\u4E0B\u56FE\u7247\u6240\u793A\u5185\u5BB9\u586B\u5199\u8868\u5355\u5E76\u70B9\u51FB\u201C\u786E\u5B9A\u201D\u6309\u94AE\u3002**
+
+![16.png](/img/blog/windows/pic/16.png)
+
+### \u8BF7\u6C42\u9A8C\u8BC1
+\`\`\`shell
+# should output a JSON object containing request data 
+curl http://localhost/get?foo=bar -H 'host: foo.bar.com'
+\`\`\`
+
+![E.png](/img/blog/windows/pic/E.png)
+
+\u66F4\u591A\u8BE6\u60C5\u4E0E\u90E8\u7F72\u65B9\u6848\u53EF\u53C2\u8003 [quick start](https://higress.cn/docs/latest/user/quickstart/)
+`},p=function(){return s},t=function(){return[{depth:2,slug:"\u524D\u7F6E\u51C6\u5907",text:"\u524D\u7F6E\u51C6\u5907"},{depth:3,slug:"\u914D\u7F6E-wsl2",text:"\u914D\u7F6E WSL2"},{depth:3,slug:"\u4E0B\u8F7D-docker-desktop",text:"\u4E0B\u8F7D Docker Desktop"},{depth:3,slug:"\u5B89\u88C5-cygwin",text:"\u5B89\u88C5 Cygwin"},{depth:4,slug:"\u9A8C\u8BC1-cygwin-\u5B89\u88C5\u662F\u5426\u6210\u529F",text:"\u9A8C\u8BC1 Cygwin \u5B89\u88C5\u662F\u5426\u6210\u529F"},{depth:4,slug:"\u4E3A-cygwin-\u914D\u7F6E\u73AF\u5883\u53D8\u91CF",text:"\u4E3A Cygwin \u914D\u7F6E\u73AF\u5883\u53D8\u91CF"},{depth:2,slug:"\u5B89\u88C5-higrees",text:"\u5B89\u88C5 Higrees"},{depth:3,slug:"\u51C6\u5907-nacos",text:"\u51C6\u5907 Nacos"},{depth:3,slug:"\u4F7F\u7528-higrees-\u5BF9\u63A5-nacos",text:"\u4F7F\u7528 Higrees \u5BF9\u63A5 Nacos"},{depth:3,slug:"higress-\u63A7\u5236\u53F0\u914D\u7F6E",text:"Higress \u63A7\u5236\u53F0\u914D\u7F6E"},{depth:3,slug:"\u8BF7\u6C42\u9A8C\u8BC1",text:"\u8BF7\u6C42\u9A8C\u8BC1"}]},e=c((f,m,b)=>{const{layout:v,...a}=n;return a.file=o,a.url=i,d`${g()}${h(s)}`})});export{e as Content,y as __tla,p as compiledContent,e as default,o as file,n as frontmatter,t as getHeadings,r as rawContent,i as url};
