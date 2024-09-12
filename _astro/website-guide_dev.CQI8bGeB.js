db29ea90ae628ba3422e22cb68e1e52d3620f7e0
johnlanni <johnlanni@users.noreply.github.com>
2024-09-12 11:24:51 +0000

    Automated deployment: Thu Sep 12 11:24:50 UTC 2024 aa0ee4cbfa9f59a45e6370176064b944ca7d411f

---
+++ _astro/website-guide_dev.CQI8bGeB.js
@@ -0,0 +1,8 @@
+import{c as a,__tla as l}from"./astro-component.cI6d52vQ.js";import{r as h,m as c,u as p,__tla as m}from"./constant.CrEfIH7u.js";import{__tla as _}from"./astro/assets-service.Da9pL5MS.js";let r,u,e,s,g,n,i,d=Promise.all([(()=>{try{return l}catch{}})(),(()=>{try{return m}catch{}})(),(()=>{try{return _}catch{}})()]).then(async()=>{let t;t=`<ol>
+<li>Higress \u7684\u7F51\u7AD9\u4ED3\u5E93\u662F <a href="https://github.com/higress-group/higress-group.github.io" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">https://github.com/higress-group/higress-group.github.io</a></li>
+<li>\u7F51\u7AD9\u6784\u5EFA\u5B8C\u6BD5\u540E\uFF0C\u5B83\u4F1A\u88AB\u81EA\u52A8\u53D1\u5E03\u5230 higress.io</li>
+</ol>`,s={title:"\u7F51\u7AD9\u5411\u5BFC",keywords:["Higress"],description:"Higress \u7F51\u7AD9\u5411\u5BFC\u3002",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/developers/committer-guide/website-guide_dev.md"},e="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/zh-cn/developers/committer-guide/website-guide_dev.md",i=void 0,n=function(){return`
+# \u7F51\u7AD9\u5411\u5BFC
+
+1. Higress \u7684\u7F51\u7AD9\u4ED3\u5E93\u662F https://github.com/higress-group/higress-group.github.io
+2. \u7F51\u7AD9\u6784\u5EFA\u5B8C\u6BD5\u540E\uFF0C\u5B83\u4F1A\u88AB\u81EA\u52A8\u53D1\u5E03\u5230 higress.io`},u=function(){return t},g=function(){return[]},r=a((b,f,w)=>{const{layout:y,...o}=s;return o.file=e,o.url=i,h`${c()}${p(t)}`})});export{r as Content,d as __tla,u as compiledContent,r as default,e as file,s as frontmatter,g as getHeadings,n as rawContent,i as url};
