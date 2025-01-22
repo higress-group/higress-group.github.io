44eeaeb28c40ba321de261c75d49c909a5e452f7
Hazel0928 <Hazel0928@users.noreply.github.com>
2025-01-22 03:10:04 +0000

    Automated deployment: Wed Jan 22 03:10:04 UTC 2025 f6054cf34f7ec107e7a995f6867026827bdb0ddc

---
+++ _astro/website-guide_dev.CPTzk07Q.js
@@ -0,0 +1,9 @@
+import{c as g,__tla as n}from"./astro-component.CXlaF79K.js";import{r as h,m as c,u as p,__tla as b}from"./constant.Bp7Qm2H_.js";import{__tla as d}from"./astro/assets-service.DQopRnBr.js";let t,u,i,r,l,a,s,m=Promise.all([(()=>{try{return n}catch{}})(),(()=>{try{return b}catch{}})(),(()=>{try{return d}catch{}})()]).then(async()=>{let e;e=`<ol>
+<li>The website repository of Higress is <a href="https://github.com/higress-group/higress-group.github.io" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">https://github.com/higress-group/higress-group.github.io</a>.</li>
+<li>After building the website, it\u2019ll be published to higress.io automatically.</li>
+</ol>`,r={title:"Website Guide",keywords:["Higress"],description:"Website Guide.",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/developers/committer-guide/website-guide_dev.md"},i="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/en/developers/committer-guide/website-guide_dev.md",s=void 0,a=function(){return`
+# Website Guide
+
+1. The website repository of Higress is https://github.com/higress-group/higress-group.github.io.
+2. After building the website, it'll be published to higress.io automatically.
+`},u=function(){return e},l=function(){return[]},t=g((_,f,y)=>{const{layout:w,...o}=r;return o.file=i,o.url=s,h`${c()}${p(e)}`})});export{t as Content,m as __tla,u as compiledContent,t as default,i as file,r as frontmatter,l as getHeadings,a as rawContent,s as url};
