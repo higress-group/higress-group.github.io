1732997adaeda8df67b2988c4ea4ee9243864a3c
johnlanni <johnlanni@users.noreply.github.com>
2025-01-23 10:05:06 +0000

    Automated deployment: Thu Jan 23 10:05:05 UTC 2025 963df72239255180ebea7d9f1b9689583392b9e7

---
+++ _astro/developers_dev.C-fEhv--.js
@@ -0,0 +1,105 @@
+const e="developers/zh-cn/developers_dev.md",t="docs",i="developers/zh-cn/developers_dev",s=`
+## \u5F00\u53D1\u4EBA\u5458
+
+\u672C\u9875\u9762\u5C55\u793A\u4E86Higress\u7684\u5F00\u53D1\u56E2\u961F\u3002\u8BF7\u901A\u8FC7\u63D0\u4EA4PR\u7684\u65B9\u5F0F\u628A\u81EA\u5DF1\u7684\u4FE1\u606F\u6DFB\u52A0\u5230\u5217\u8868\u4E0A\u3002
+
+**\u6CE8\uFF1A\u6392\u540D\u6309 github id \u9996\u5B57\u6BCD**
+
+## Higress Maintainers
+
+| \u59D3\u540D  | github                        | \u516C\u53F8\u7EC4\u7EC7             |
+|-----|-------------------------------| --------------- |
+| \u5434\u65B0\u519B | [2456868764](https://github.com/2456868764) | \u72EC\u7ACB\u5F00\u53D1\u8005      |
+| \u8463\u827A\u8343 | [CH3CHO](https://github.com/CH3CHO) | \u643A\u7A0B        |
+| \u803F\u857E\u857E | [gengleilei](https://github.com/gengleilei) | \u963F\u91CC\u4E91        |
+| \u5F20\u6DFB\u7FFC | [johnlanni](https://github.com/johnlanni) | \u963F\u91CC\u4E91        |
+| \u5218\u6653\u745E | [rinfx](https://github.com/rinfx) | \u963F\u91CC\u4E91        |
+| \u5B63\u654F | [slievrly](https://github.com/slievrly) | \u963F\u91CC\u4E91        |
+
+## Higress Approvers
+| \u59D3\u540D  | github                        | \u516C\u53F8\u7EC4\u7EC7             |
+|-----|-------------------------------| --------------- |
+| \u97E9\u8D24\u6D9B | [hanxiantao](https://github.com/hanxiantao)         | \u4FE1\u4E5F\u79D1\u6280      |
+| \u7A0B\u6CBB\u73AE | [Se7en](https://github.com/cr7258)   | SAP  |
+| \u8303\u626C | [SpecialYang](https://github.com/SpecialYang) | \u5B57\u8282\u8DF3\u52A8        |
+| \u5218\u8BAD\u707C | [Xunzhuo](https://github.com/Xunzhuo) | \u817E\u8BAF        |
+
+## Higress Reviewers
+| \u59D3\u540D  | github                        | \u516C\u53F8\u7EC4\u7EC7             |
+|-----|-------------------------------| --------------- |
+| \u51CC\u8F76\u7FA4 | [Lynskylate](https://github.com/Lynskylate) | eBay  |
+| \u8D75\u70B3\u5803 | [sjtuzbk](https://github.com/sjtuzbk) | \u963F\u91CC\u4E91        |
+| \u97E6\u946B | [WeixinX](https://github.com/weixinx) | \u7231\u6A59\u79D1\u6280   |
+
+## Higress Members
+| \u59D3\u540D   | github                                          | \u516C\u53F8\u7EC4\u7EC7     |
+|--------|-------------------------------------------------|----------|
+| \u5218\u6BC5\u6770 | [007gzs](https://github.com/007gzs) | \u68F1\u955C\u4E03\u5F69 |
+| \u5F20\u6B63\u597D | [alexzzh](https://github.com/alexzzh)   | \u683C\u5C14\u8F6F\u4EF6  |
+| \u674E\u5F3A\u6797 | [Charlie17Li](https://github.com/Charlie17Li)   | \u6D59\u5927  |
+| \u5C01\u5B87\u817E | [Ffyyt](https://github.com/Fkbqf)               | \u897F\u5B89\u90AE\u7535     |
+| \u7530\u4E9A\u6D9B | [Hinsteny](https://github.com/Hinsteny)         | \u8682\u8681      |
+| \u5C48\u6657\u715C | [Ink-33](https://github.com/Ink-33)         | \u4E1C\u839E\u7406\u5DE5     |
+| \u738B\u5C0F\u5EFA | [littlejian](https://github.com/littlejiancc)      | \u91D1\u667A\u5854     |
+| \u90B5\u4F73\u6210 | [sjcsjc123](https://github.com/sjcsjc123) | \u676D\u7535 |
+| \u5B8B\u9E4F\u8FDC | [songpengyuan](https://github.com/songpengyuan) | \u5C0F\u6EE1\u79D1\u6280 |
+| \u5F6D\u4E07\u5C71 | [Uncle-Justice](https://github.com/Uncle-Justice) | \u534E\u4E2D\u79D1\u6280 |
+| \u8D75\u4F1F\u57FA | [vikizhao156](https://github.com/vikizhao156)   | \u4E0A\u4EA4     |
+| \u90A2\u4E91\u9633 | [xingyunyang01](https://github.com/xingyunyang01)   | \u8054\u901A\u6570\u79D1     |
+
+
+## \u83B7\u53D6Higress\u5F00\u53D1\u8005\u5934\u8854
+
+**\u5934\u8854\u672C\u8EAB\u662F\u4E00\u79CD\u8363\u8A89\u8C61\u5F81\uFF0C\u6BCF\u4E00\u4F4D\u53C2\u4E0EHigress\u7684\u8D21\u732E\u8005\u5728\u5F00\u6E90\u793E\u533A\u7684\u5730\u4F4D\u90FD\u662F\u5E73\u7B49\u7684**
+
+\u6BCF\u79CD\u7684\u5934\u8854\u7684\u83B7\u53D6\u65B9\u5F0F\u5982\u4E0B\uFF1A
+
+### Member \u5934\u8854
+
+**\u6761\u4EF6\uFF1A**
+- \u5B8C\u6210\u4EFB\u610F issue
+
+> Higress\u5F00\u6E90\u793E\u533A\u4E3A\u6BCF\u4E2A\u53EF\u8BA4\u9886\uFF08help wanted\u6807\u7B7E\uFF09\u7684issue\u90FD\u6807\u8BB0\u4E86\u96BE\u5EA6\u6807\u7B7E\uFF1A\u7B80\u5355(level/easy)\uFF0C\u666E\u901A(level/normal)\uFF0C\u6709\u6311\u6218(level/challenge)\uFF0C\u5B8C\u6210issue\u540E\u53EF\u4EE5\u83B7\u5F97\u5BF9\u5E94\u79EF\u5206
+
+- \u548C Higress \u4EFB\u610F Maintainer \u4FDD\u6301\u8054\u7CFB\uFF0C\u6709\u610F\u613F\u957F\u671F\u53C2\u4E0E\u793E\u533A\u8D21\u732E
+
+
+**\u7533\u8BF7\u65B9\u5F0F\uFF1A**
+
+\u76F4\u63A5\u8054\u7CFB Maintainer \u7533\u8BF7
+
+### Reviewer \u5934\u8854
+
+**\u6761\u4EF6\uFF1A**
+- \u6EE1\u8DB3Member\u6761\u4EF6
+- \u5728\u4E00\u4E9B\u6838\u5FC3 issue \u4E2D\u505A\u51FA\u8D21\u732E
+
+**\u7533\u8BF7\u65B9\u5F0F\uFF1A**
+
+\u7531Approvers/Maintainer\u63D0\u540D
+
+
+### Approver \u5934\u8854
+
+**\u6761\u4EF6\uFF1A**
+- \u6EE1\u8DB3Reviewer\u6761\u4EF6
+- \u53EF\u4EE5\u4E3B\u5BFC\u4E00\u4E2A\u6280\u672F\u65B9\u5411\uFF0C\u5E76\u63A8\u8FDB\u8BB8\u591A\u6838\u5FC3issue\u5B8C\u6210
+
+**\u7533\u8BF7\u65B9\u5F0F\uFF1A**
+
+\u7531Maintainer\u63D0\u540D
+
+
+### Maintainer \u5934\u8854
+
+Maintainer\u662F\u5BF9Higress\u9879\u76EE\uFF08\u5305\u62ECHigress\u4E0B\u7684\u9879\u76EE\uFF09\u7684\u6F14\u8FDB\u548C\u53D1\u5C55\u505A\u51FA\u663E\u8457\u8D21\u732E\u7684\u4E2A\u4EBA\u3002\u5728\u793E\u533A\u4E2D\u5177\u6709\u6709\u76EE\u5171\u7779\u7684\u5F71\u54CD\u529B\uFF0C\u80FD\u591F\u4EE3\u8868Higress\u53C2\u52A0\u91CD\u8981\u7684\u793E\u533A\u4F1A\u8BAE\u548C\u6D3B\u52A8\u3002
+
+**\u6761\u4EF6\uFF1A**
+- \u6EE1\u8DB3Approver\u6761\u4EF6
+- \u4E3B\u5BFC\u591A\u4E2A\u6280\u672F\u65B9\u5411\uFF0C\u7EC4\u7EC7\u5E76\u63A8\u8FDB\u6838\u5FC3issue\u5B8C\u6210
+- \u79EF\u6781\u53C2\u4E0E\u5F00\u6E90\u793E\u533A\u7684\u65E5\u5E38\u5DE5\u4F5C\uFF1A\u5F15\u5BFC\u65B0\u4EBA\u5F00\u53D1\uFF0C\u7528\u6237\u95EE\u9898\u89E3\u7B54\u7B49
+
+**\u7533\u8BF7\u65B9\u5F0F\uFF1A**
+
+\u75312\u540DMaintainer\u63D0\u540D
+`,h={title:"\u5F00\u53D1\u4EBA\u5458",description:"Higress \u7EF4\u62A4\u8005\u540D\u5355",editUrl:!0,head:[],template:"doc",sidebar:{hidden:!1,attrs:{}},pagefind:!0,keywords:["Higress","\u7EF4\u62A4\u8005"]},n={type:"content",filePath:"/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/developers/zh-cn/developers_dev.md",rawData:void 0};export{n as _internal,s as body,t as collection,h as data,e as id,i as slug};
