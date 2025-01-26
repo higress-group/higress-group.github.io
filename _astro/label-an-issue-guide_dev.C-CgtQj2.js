a7f3f9dffc3cb691dfc35799e3fbe2ae39760823
Hazel0928 <Hazel0928@users.noreply.github.com>
2025-01-26 02:37:37 +0000

    Automated deployment: Sun Jan 26 02:37:37 UTC 2025 f7a8942f0a2d4e193e1c0bb92807b7a16fe4bd1f

---
+++ _astro/label-an-issue-guide_dev.C-CgtQj2.js
@@ -0,0 +1,70 @@
+import{c as u,__tla as p}from"./astro-component.CXlaF79K.js";import{r as h,m,u as c,__tla as g}from"./constant.C0h0vmZ9.js";import{__tla as d}from"./astro/assets-service.DQopRnBr.js";let t,a,i,r,o,n,s,y=Promise.all([(()=>{try{return p}catch{}})(),(()=>{try{return g}catch{}})(),(()=>{try{return d}catch{}})()]).then(async()=>{let e;e=`<p>If you are handling an issue, remember to <strong>mark the issue clearly with one or more labels</strong> whenever you think it\u2019s meaningful. With labels on, other developers can easily recognize problems, classify them or track progress.</p>
+<p>For issues or pull requests that need coding and further version release to fix, you should always <strong>mark it with a <a href="https://github.com/alibaba/higress/milestones" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">milestone</a></strong>.</p>
+<p>Some frequently used labels:</p>
+<ul>
+<li>
+<p>Help Wanted</p>
+<ul>
+<li>help wanted</li>
+<li>good first issue</li>
+</ul>
+</li>
+<li>
+<p>Priority</p>
+<ul>
+<li>priority/blocker</li>
+<li>priority/high</li>
+<li>priority/low</li>
+<li>priority/normal</li>
+</ul>
+</li>
+<li>
+<p>Status</p>
+<ul>
+<li>status/need-triage</li>
+<li>status/DO-NOT-MERGE</li>
+<li>status/READY-TO-MERGE</li>
+<li>status/invalid</li>
+<li>status/wontfix</li>
+</ul>
+</li>
+<li>
+<p>Type</p>
+<ul>
+<li>type/bug</li>
+<li>type/documentation</li>
+<li>type/enhancement</li>
+<li>type/feature</li>
+</ul>
+</li>
+</ul>`,r={title:"Label an Issue",keywords:["Higress"],description:"Label an Issue.",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/developers/committer-guide/label-an-issue-guide_dev.md"},i="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/en/developers/committer-guide/label-an-issue-guide_dev.md",s=void 0,n=function(){return`
+# Label an Issue
+
+If you are handling an issue, remember to **mark the issue clearly with one or more labels** whenever you think it's meaningful. With labels on, other developers can easily recognize problems, classify them or track progress. 
+
+For issues or pull requests that need coding and further version release to fix, you should always **mark it with a [milestone](https://github.com/alibaba/higress/milestones)**. 
+
+Some frequently used labels:
+* Help Wanted
+  * help wanted
+  * good first issue
+
+* Priority
+  * priority/blocker
+  * priority/high
+  * priority/low
+  * priority/normal
+
+* Status
+  * status/need-triage
+  * status/DO-NOT-MERGE
+  * status/READY-TO-MERGE
+  * status/invalid 
+  * status/wontfix
+
+* Type
+  * type/bug
+  * type/documentation
+  * type/enhancement
+  * type/feature
+`},a=function(){return e},o=function(){return[]},t=u((b,f,_)=>{const{layout:w,...l}=r;return l.file=i,l.url=s,h`${m()}${c(e)}`})});export{t as Content,y as __tla,a as compiledContent,t as default,i as file,r as frontmatter,o as getHeadings,n as rawContent,s as url};
