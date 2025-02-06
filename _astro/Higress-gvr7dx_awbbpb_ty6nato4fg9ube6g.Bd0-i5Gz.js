3815eb7755f3c99c0705bf0fc55a43fba601911d
Hazel0928 <Hazel0928@users.noreply.github.com>
2025-02-06 05:57:12 +0000

    Automated deployment: Thu Feb  6 05:57:12 UTC 2025 b66e156386ea7e6716c8a61561c3d55b0beecfed

---
+++ _astro/Higress-gvr7dx_awbbpb_ty6nato4fg9ube6g.Bd0-i5Gz.js
@@ -0,0 +1,35 @@
+import{c as i,__tla as a}from"./astro-component.CXlaF79K.js";import{r as c,m as p,u as y,__tla as b}from"./constant.DwVJL5w2.js";import{__tla as u}from"./astro/assets-service.DQopRnBr.js";let t,s,r,n,g,f,l,m=Promise.all([(()=>{try{return a}catch{}})(),(()=>{try{return b}catch{}})(),(()=>{try{return u}catch{}})()]).then(async()=>{let o;o=`<p><img src="/img/1738821195621.png" alt="" referrerpolicy="no-referrer"></p>
+<p><font style="color:rgb(24, 24, 24);">\u9080\u60A8\u4E00\u8D77\u4EA4\u6D41\uFF0C\u63A2\u7D22 AI \u539F\u751F\u5E94\u7528\u7684\u5DE5\u7A0B\u5316\u843D\u5730\uFF01</font></p>
+<p><strong><font style="color:rgb(46, 75, 227);">\u4E00\u3001\u6D3B\u52A8\u7B80\u4ECB</font></strong></p>
+<p><font style="color:rgb(24, 24, 24);">Al \u9A71\u52A8\u7684\u5E94\u7528\u7A0B\u5E8F\u5F00\u53D1\u3001\u90E8\u7F72\u548C\u8FD0\u7EF4\uFF0C\u7ED9\u5E94\u7528\u5E26\u6765\u4E86\u65B0\u7684\u751F\u547D\u529B\u548C\u60F3\u8C61\u7A7A\u95F4\u3002\u4F46\u5927\u90E8\u5206\u4F01\u4E1A\u3001\u5C24\u5176\u662F\u4E2D\u5C0F\u578B\u4F01\u4E1A\u5BF9 Al \u5E94\u7528\u7684\u7F16\u7A0B\u6846\u67B6\u3001\u53EF\u89C2\u6D4B\u4F53\u7CFB\u3001\u7F51\u5173\u67B6\u6784\u7684\u8BBE\u8BA1\u548C\u4F7F\u7528\u4F9D\u65E7\u4E0D\u592A\u719F\u7EC3\uFF0C\u751F\u4EA7\u73AF\u5883\u4E0B\u7684\u5DE5\u7A0B\u5316\u5B9E\u8DF5\u4ECD\u4E0D\u592A\u4E30\u5BCC\u3002\u4E3A\u6B64\u6211\u4EEC\u9080\u8BF7\u4E86\u6DF1\u8015 Al \u5E94\u7528\u5DE5\u7A0B\u5316\u7684\u4E00\u7EBF\u7814\u53D1\u5DE5\u7A0B\u5E08\uFF0C\u5206\u4EAB\u4ED6\u4EEC\u7684\u751F\u4EA7\u7ECF\u9A8C\uFF0C\u4EE5\u53CA Spring Al Alibaba \u548C Higress \u7B49\u5F00\u6E90\u9879\u76EE\u7684\u6700\u65B0\u8FDB\u5C55\uFF0C\u5E76\u901A\u8FC7\u73B0\u573A\u5B9E\u64CD\u52A0\u56FA\u5B66\u4E60\u6548\u679C\u3002\u672C\u6B21\u6D3B\u52A8\u56E0\u573A\u5730\u9650\u5236\uFF0C\u91C7\u53D6\u62A5\u540D\u5BA1\u6838\u5236\u5EA6\uFF0C\u73B0\u573A\u5B8C\u6210\u5B9E\u64CD\uFF08\u9700\u643A\u5E26\u7535\u8111\uFF09\uFF0C\u9881\u53D1\u4E13\u5C5E\u8BC1\u4E66\u3002</font></p>
+<ul>
+<li><strong><font style="color:rgb(24, 24, 24);">\u6D3B\u52A8\u65F6\u95F4\uFF1A</font><font style="color:rgb(46, 75, 227);">2024 \u5E74 11 \u6708 29 \u65E5\uFF08\u5468\u4E94\uFF0913:00-17:30</font></strong></li>
+<li><strong><font style="color:rgb(24, 24, 24);">\u6D3B\u52A8\u5730\u5740\uFF1A</font><font style="color:rgb(46, 75, 227);">\u6DF1\u5733\u5E02\u5357\u5C71\u533A\u79D1\u82D1\u5357\u8DEF 3331 \u53F7\u963F\u91CC\u4E2D\u5FC3\uFF0CT1-3-1-E \u9752\u4E91\u6DA7</font></strong></li>
+<li><strong><font style="color:rgb(24, 24, 24);">\u6D3B\u52A8\u5730\u5740\uFF1A</font></strong><a href="https://hd.aliyun.com/form/5292" referrerpolicy="unsafe-url" rel="nofollow" target="_blank"><strong><font style="color:rgb(46, 75, 227);">https://hd.aliyun.com/form/5292</font></strong></a></li>
+</ul>
+<p><strong><font style="color:rgb(46, 75, 227);">\u4E8C\u3001\u8BE6\u7EC6\u8BAE\u7A0B</font></strong></p>
+<p><img src="/img/1738821198077.png" alt="" referrerpolicy="no-referrer"></p>
+<p><strong><font style="color:rgb(46, 75, 227);">\u4E09\u3001\u5F80\u671F\u56DE\u987E</font></strong></p>
+<p><img src="/img/1738821200274.png" alt="" referrerpolicy="no-referrer"></p>`,n={title:"Higress@\u6DF1\u5733\uFF0CAI \u5E94\u7528\u4F01\u4E1A\u5F00\u53D1\u8005\u6C99\u9F99\u9080\u60A8\u53C2\u4F1A",description:"Higress@\u6DF1\u5733\uFF0CAI \u5E94\u7528\u4F01\u4E1A\u5F00\u53D1\u8005\u6C99\u9F99\u9080\u60A8\u53C2\u4F1A",date:"2024-11-26",category:"activity-preview",keywords:["Higress"],authors:"CH3CHO"},r="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/blog/Higress-gvr7dx_awbbpb_ty6nato4fg9ube6g.md",l=void 0,f=function(){return`
+![](/img/1738821195621.png)
+
+<font style="color:rgb(24, 24, 24);">\u9080\u60A8\u4E00\u8D77\u4EA4\u6D41\uFF0C\u63A2\u7D22 AI \u539F\u751F\u5E94\u7528\u7684\u5DE5\u7A0B\u5316\u843D\u5730\uFF01</font>
+
+**<font style="color:rgb(46, 75, 227);">\u4E00\u3001\u6D3B\u52A8\u7B80\u4ECB</font>**
+
+<font style="color:rgb(24, 24, 24);">Al \u9A71\u52A8\u7684\u5E94\u7528\u7A0B\u5E8F\u5F00\u53D1\u3001\u90E8\u7F72\u548C\u8FD0\u7EF4\uFF0C\u7ED9\u5E94\u7528\u5E26\u6765\u4E86\u65B0\u7684\u751F\u547D\u529B\u548C\u60F3\u8C61\u7A7A\u95F4\u3002\u4F46\u5927\u90E8\u5206\u4F01\u4E1A\u3001\u5C24\u5176\u662F\u4E2D\u5C0F\u578B\u4F01\u4E1A\u5BF9 Al \u5E94\u7528\u7684\u7F16\u7A0B\u6846\u67B6\u3001\u53EF\u89C2\u6D4B\u4F53\u7CFB\u3001\u7F51\u5173\u67B6\u6784\u7684\u8BBE\u8BA1\u548C\u4F7F\u7528\u4F9D\u65E7\u4E0D\u592A\u719F\u7EC3\uFF0C\u751F\u4EA7\u73AF\u5883\u4E0B\u7684\u5DE5\u7A0B\u5316\u5B9E\u8DF5\u4ECD\u4E0D\u592A\u4E30\u5BCC\u3002\u4E3A\u6B64\u6211\u4EEC\u9080\u8BF7\u4E86\u6DF1\u8015 Al \u5E94\u7528\u5DE5\u7A0B\u5316\u7684\u4E00\u7EBF\u7814\u53D1\u5DE5\u7A0B\u5E08\uFF0C\u5206\u4EAB\u4ED6\u4EEC\u7684\u751F\u4EA7\u7ECF\u9A8C\uFF0C\u4EE5\u53CA Spring Al Alibaba \u548C Higress \u7B49\u5F00\u6E90\u9879\u76EE\u7684\u6700\u65B0\u8FDB\u5C55\uFF0C\u5E76\u901A\u8FC7\u73B0\u573A\u5B9E\u64CD\u52A0\u56FA\u5B66\u4E60\u6548\u679C\u3002\u672C\u6B21\u6D3B\u52A8\u56E0\u573A\u5730\u9650\u5236\uFF0C\u91C7\u53D6\u62A5\u540D\u5BA1\u6838\u5236\u5EA6\uFF0C\u73B0\u573A\u5B8C\u6210\u5B9E\u64CD\uFF08\u9700\u643A\u5E26\u7535\u8111\uFF09\uFF0C\u9881\u53D1\u4E13\u5C5E\u8BC1\u4E66\u3002</font>
+
++ **<font style="color:rgb(24, 24, 24);">\u6D3B\u52A8\u65F6\u95F4\uFF1A</font><font style="color:rgb(46, 75, 227);">2024 \u5E74 11 \u6708 29 \u65E5\uFF08\u5468\u4E94\uFF0913:00-17:30</font>**
++ **<font style="color:rgb(24, 24, 24);">\u6D3B\u52A8\u5730\u5740\uFF1A</font><font style="color:rgb(46, 75, 227);">\u6DF1\u5733\u5E02\u5357\u5C71\u533A\u79D1\u82D1\u5357\u8DEF 3331 \u53F7\u963F\u91CC\u4E2D\u5FC3\uFF0CT1-3-1-E \u9752\u4E91\u6DA7</font>**
++ **<font style="color:rgb(24, 24, 24);">\u6D3B\u52A8\u5730\u5740\uFF1A</font>**[**<font style="color:rgb(46, 75, 227);">https://hd.aliyun.com/form/5292</font>**](https://hd.aliyun.com/form/5292)
+
+**<font style="color:rgb(46, 75, 227);">\u4E8C\u3001\u8BE6\u7EC6\u8BAE\u7A0B</font>**
+
+![](/img/1738821198077.png)
+
+**<font style="color:rgb(46, 75, 227);">\u4E09\u3001\u5F80\u671F\u56DE\u987E</font>**
+
+![](/img/1738821200274.png)
+
+
+`},s=function(){return o},g=function(){return[]},t=i((h,_,A)=>{const{layout:d,...e}=n;return e.file=r,e.url=l,c`${p()}${y(o)}`})});export{t as Content,m as __tla,s as compiledContent,t as default,r as file,n as frontmatter,g as getHeadings,f as rawContent,l as url};
---
+++ _astro/ip-restriction.BWNPg8eo.js
@@ -0,0 +1,95 @@
+import{c,__tla as p}from"./astro-component.CXlaF79K.js";import{r as l,m as u,u as h,__tla as y}from"./constant.DwVJL5w2.js";import{__tla as g}from"./astro/assets-service.DQopRnBr.js";let t,d,s,i,r,o,a,v=Promise.all([(()=>{try{return p}catch{}})(),(()=>{try{return y}catch{}})(),(()=>{try{return g}catch{}})()]).then(async()=>{let e;e=`<h2 id="function-description">Function Description</h2>
+<p>The <code dir="auto">ip-restriction</code> plugin can restrict access to services or routes by whitelisting or blacklisting IP addresses. It supports restrictions on a single IP address, multiple IP addresses, and CIDR ranges like 10.10.10.0/24.</p>
+<h2 id="running-attributes">Running Attributes</h2>
+<p>Plugin execution phase: <code dir="auto">Authentication Phase</code></p>
+<p>Plugin execution priority: <code dir="auto">210</code></p>
+<h2 id="configuration-description">Configuration Description</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<table><thead><tr><th>Configuration Item</th><th>Type</th><th>Required</th><th>Default Value</th><th>Description</th></tr></thead><tbody><tr><td>ip_source_type</td><td>string</td><td>No</td><td>origin-source</td><td>Optional values: 1. Peer socket IP: <code dir="auto">origin-source</code>; 2. Get from header: <code dir="auto">header</code></td></tr><tr><td>ip_header_name</td><td>string</td><td>No</td><td>x-forwarded-for</td><td>When <code dir="auto">ip_source_type</code> is <code dir="auto">header</code>, specify the custom IP source header</td></tr><tr><td>allow</td><td>array</td><td>No</td><td>[]</td><td>Whitelist</td></tr><tr><td>deny</td><td>array</td><td>No</td><td>[]</td><td>Blacklist</td></tr><tr><td>status</td><td>int</td><td>No</td><td>403</td><td>HTTP status code when access is denied</td></tr><tr><td>message</td><td>string</td><td>No</td><td>Your IP address is blocked.</td><td>Return message when access is denied</td></tr></tbody></table>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#85E89D">ip_source_type</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">origin-source</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">allow</span><span style="--0:#E1E4E8">:</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#79B8FF">10.0.0.1</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#9ECBFF">192.168.0.0/16</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="ip_source_type: origin-source\x7Fallow:\x7F  - 10.0.0.1\x7F  - 192.168.0.0/16"><div></div></button></div></figure></div>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#85E89D">ip_source_type</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">header</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">ip_header_name</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">x-real-iP</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">deny</span><span style="--0:#E1E4E8">:</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#79B8FF">10.0.0.1</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">- </span><span style="--0:#9ECBFF">192.169.0.0/16</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="ip_source_type: header\x7Fip_header_name: x-real-iP\x7Fdeny:\x7F  - 10.0.0.1\x7F  - 192.169.0.0/16"><div></div></button></div></figure></div>`,i={title:"IP Access Restriction",keywords:["higress","ip restriction"],description:"IP access restriction plugin configuration reference"},s="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/en/plugins/security/ip-restriction.md",a=void 0,o=function(){return`## Function Description
+The \`ip-restriction\` plugin can restrict access to services or routes by whitelisting or blacklisting IP addresses. It supports restrictions on a single IP address, multiple IP addresses, and CIDR ranges like 10.10.10.0/24.
+
+## Running Attributes
+Plugin execution phase: \`Authentication Phase\`
+
+Plugin execution priority: \`210\`
+
+## Configuration Description
+| Configuration Item  | Type    | Required | Default Value                   | Description                                 |
+|---------------------|---------|----------|---------------------------------|---------------------------------------------|
+| ip_source_type      | string  | No       | origin-source                   | Optional values: 1. Peer socket IP: \`origin-source\`; 2. Get from header: \`header\` |
+| ip_header_name      | string  | No       | x-forwarded-for                | When \`ip_source_type\` is \`header\`, specify the custom IP source header            |
+| allow               | array   | No       | []                              | Whitelist                                    |
+| deny                | array   | No       | []                              | Blacklist                                    |
+| status              | int     | No       | 403                             | HTTP status code when access is denied      |
+| message             | string  | No       | Your IP address is blocked.     | Return message when access is denied         |
+
+\`\`\`yaml
+ip_source_type: origin-source
+allow:
+  - 10.0.0.1
+  - 192.168.0.0/16
+\`\`\`
+
+\`\`\`yaml
+ip_source_type: header
+ip_header_name: x-real-iP
+deny:
+  - 10.0.0.1
+  - 192.169.0.0/16
+\`\`\`
+`},d=function(){return e},r=function(){return[{depth:2,slug:"function-description",text:"Function Description"},{depth:2,slug:"running-attributes",text:"Running Attributes"},{depth:2,slug:"configuration-description",text:"Configuration Description"}]},t=c((f,E,_)=>{const{layout:m,...n}=i;return n.file=s,n.url=a,l`${u()}${h(e)}`})});export{t as Content,v as __tla,d as compiledContent,t as default,s as file,i as frontmatter,r as getHeadings,o as rawContent,a as url};
