c4c1ad18495c4640b5d92cb5540bc93cb4f090c5
heimanba <heimanba@users.noreply.github.com>
2025-02-14 04:23:37 +0000

    Automated deployment: Fri Feb 14 04:23:36 UTC 2025 007dc0e3af179152dee04ac83a0a5ea95082fa79

---
+++ _astro/annotation.CrIX1sUx.js
@@ -0,0 +1,6 @@
+import{c as g,__tla as c}from"./astro-component.CXlaF79K.js";import{r as l,m,u as d,__tla as h}from"./constant.CwnOaDOB.js";import{__tla as _}from"./astro/assets-service.DQopRnBr.js";let r,n,e,s,o,u,a,p=Promise.all([(()=>{try{return c}catch{}})(),(()=>{try{return h}catch{}})(),(()=>{try{return _}catch{}})()]).then(async()=>{let t;t=`<h1 id="higress-parameters-guide">Higress Parameters Guide</h1>
+<p>TBD</p>`,s={title:"Higress Annotation",keywords:["Higress"],description:"Higress Parameters Guide\u3002",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/user/annotation.md"},e="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/en/user/annotation.md",a=void 0,u=function(){return`
+# Higress Parameters Guide
+
+TBD
+`},n=function(){return t},o=function(){return[{depth:1,slug:"higress-parameters-guide",text:"Higress Parameters Guide"}]},r=g((f,H,b)=>{const{layout:y,...i}=s;return i.file=e,i.url=a,l`${m()}${d(t)}`})});export{r as Content,p as __tla,n as compiledContent,r as default,e as file,s as frontmatter,o as getHeadings,u as rawContent,a as url};
