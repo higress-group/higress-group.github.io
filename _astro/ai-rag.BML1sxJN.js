36d880e3f6a0c8dfa71b0cc23dcbf3769aae47d6
Hazel0928 <Hazel0928@users.noreply.github.com>
2025-02-18 03:32:03 +0000

    Automated deployment: Tue Feb 18 03:32:02 UTC 2025 1d002b0b9fc46edd1648a1a2d4fbf6109964f118

---
+++ _astro/ai-rag.BML1sxJN.js
@@ -0,0 +1,172 @@
+import{c as r,__tla as p}from"./astro-component.CXlaF79K.js";import{r as x,m as l,u as v,__tla as h}from"./constant.DAvxYmA1.js";import{__tla as u}from"./astro/assets-service.DQopRnBr.js";let t,c,e,d,n,o,a,y=Promise.all([(()=>{try{return p}catch{}})(),(()=>{try{return h}catch{}})(),(()=>{try{return u}catch{}})()]).then(async()=>{let s;s=`<h2 id="\u529F\u80FD\u8BF4\u660E">\u529F\u80FD\u8BF4\u660E</h2>
+<p>\u901A\u8FC7\u5BF9\u63A5\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u5B9E\u73B0LLM-RAG\uFF0C\u6D41\u7A0B\u5982\u56FE\u6240\u793A\uFF1A</p>
+<img src="https://img.alicdn.com/imgextra/i1/O1CN01LuRVs41KhoeuzakeF_!!6000000001196-0-tps-1926-1316.jpg" width="600">
+<h2 id="\u8FD0\u884C\u5C5E\u6027">\u8FD0\u884C\u5C5E\u6027</h2>
+<p>\u63D2\u4EF6\u6267\u884C\u9636\u6BB5\uFF1A<code dir="auto">\u9ED8\u8BA4\u9636\u6BB5</code>
+\u63D2\u4EF6\u6267\u884C\u4F18\u5148\u7EA7\uFF1A<code dir="auto">400</code></p>
+<h2 id="\u914D\u7F6E\u8BF4\u660E">\u914D\u7F6E\u8BF4\u660E</h2>
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+<table><thead><tr><th>\u540D\u79F0</th><th>\u6570\u636E\u7C7B\u578B</th><th>\u586B\u5199\u8981\u6C42</th><th>\u9ED8\u8BA4\u503C</th><th>\u63CF\u8FF0</th></tr></thead><tbody><tr><td><code dir="auto">dashscope.apiKey</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u7528\u4E8E\u5728\u8BBF\u95EE\u901A\u4E49\u5343\u95EE\u670D\u52A1\u65F6\u8FDB\u884C\u8BA4\u8BC1\u7684\u4EE4\u724C\u3002</td></tr><tr><td><code dir="auto">dashscope.serviceFQDN</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u901A\u4E49\u5343\u95EE\u670D\u52A1\u540D</td></tr><tr><td><code dir="auto">dashscope.servicePort</code></td><td>int</td><td>\u5FC5\u586B</td><td>-</td><td>\u901A\u4E49\u5343\u95EE\u670D\u52A1\u7AEF\u53E3</td></tr><tr><td><code dir="auto">dashscope.serviceHost</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u8BBF\u95EE\u901A\u4E49\u5343\u95EE\u670D\u52A1\u65F6\u57DF\u540D</td></tr><tr><td><code dir="auto">dashvector.apiKey</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u7528\u4E8E\u5728\u8BBF\u95EE\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u65F6\u8FDB\u884C\u8BA4\u8BC1\u7684\u4EE4\u724C\u3002</td></tr><tr><td><code dir="auto">dashvector.serviceFQDN</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u540D</td></tr><tr><td><code dir="auto">dashvector.servicePort</code></td><td>int</td><td>\u5FC5\u586B</td><td>-</td><td>\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u7AEF\u53E3</td></tr><tr><td><code dir="auto">dashvector.serviceHost</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u8BBF\u95EE\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u65F6\u57DF\u540D</td></tr><tr><td><code dir="auto">dashvector.topk</code></td><td>int</td><td>\u5FC5\u586B</td><td>-</td><td>\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u65F6\u83B7\u53D6\u5411\u91CF\u6570</td></tr><tr><td><code dir="auto">dashvector.threshold</code></td><td>float</td><td>\u5FC5\u586B</td><td>-</td><td>\u5411\u91CF\u8DDD\u79BB\u9608\u503C\uFF0C\u9AD8\u4E8E\u8BE5\u9608\u503C\u7684\u6587\u6863\u4F1A\u88AB\u8FC7\u6EE4\u6389</td></tr><tr><td><code dir="auto">dashvector.field</code></td><td>string</td><td>\u5FC5\u586B</td><td>-</td><td>\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u5B58\u50A8\u6587\u6863\u7684\u5B57\u6BB5\u540D</td></tr></tbody></table>
+<p>\u63D2\u4EF6\u5F00\u542F\u540E\uFF0C\u5728\u4F7F\u7528\u94FE\u8DEF\u8FFD\u8E2A\u529F\u80FD\u65F6\uFF0C\u4F1A\u5728span\u7684attribute\u4E2D\u6DFB\u52A0rag\u68C0\u7D22\u5230\u7684\u6587\u6863id\u4FE1\u606F\uFF0C\u4F9B\u6392\u67E5\u95EE\u9898\u4F7F\u7528\u3002</p>
+<h2 id="\u793A\u4F8B">\u793A\u4F8B</h2>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#85E89D">dashscope</span><span style="--0:#E1E4E8">:</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">apiKey</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">xxxxxxxxxxxxxxx</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">serviceFQDN</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">dashscope</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">servicePort</span><span style="--0:#E1E4E8">: </span><span style="--0:#79B8FF">443</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">serviceHost</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">dashscope.aliyuncs.com</span></div></div><div class="ec-line"><div class="code"><span style="--0:#85E89D">dashvector</span><span style="--0:#E1E4E8">:</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">apiKey</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">xxxxxxxxxxxxxxxxxxxx</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">serviceFQDN</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">dashvector</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">servicePort</span><span style="--0:#E1E4E8">: </span><span style="--0:#79B8FF">443</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">serviceHost</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">vrs-cn-xxxxxxxxxxxxxxx.dashvector.cn-hangzhou.aliyuncs.com</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">collection</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">xxxxxxxxxxxxxxx</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">topk</span><span style="--0:#E1E4E8">: </span><span style="--0:#79B8FF">1</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">threshold</span><span style="--0:#E1E4E8">: </span><span style="--0:#79B8FF">0.4</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#85E89D">field</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">raw</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="dashscope:\x7F    apiKey: xxxxxxxxxxxxxxx\x7F    serviceFQDN: dashscope\x7F    servicePort: 443\x7F    serviceHost: dashscope.aliyuncs.com\x7Fdashvector:\x7F    apiKey: xxxxxxxxxxxxxxxxxxxx\x7F    serviceFQDN: dashvector\x7F    servicePort: 443\x7F    serviceHost: vrs-cn-xxxxxxxxxxxxxxx.dashvector.cn-hangzhou.aliyuncs.com\x7F    collection: xxxxxxxxxxxxxxx\x7F    topk: 1\x7F    threshold: 0.4\x7F    field: raw"><div></div></button></div></figure></div>
+<p><a href="https://github.com/shijiebei2009/CEC-Corpus" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">CEC-Corpus</a> \u6570\u636E\u96C6\u5305\u542B 332 \u7BC7\u7A81\u53D1\u4E8B\u4EF6\u7684\u65B0\u95FB\u62A5\u9053\u7684\u8BED\u6599\u548C\u6807\u6CE8\u6570\u636E\uFF0C\u63D0\u53D6\u5176\u539F\u59CB\u7684\u65B0\u95FB\u7A3F\u6587\u672C\uFF0C\u5C06\u5176\u5411\u91CF\u5316\u540E\u6DFB\u52A0\u5230\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u3002\u6587\u672C\u5411\u91CF\u5316\u7684\u6559\u7A0B\u53EF\u4EE5\u53C2\u8003<a href="https://help.aliyun.com/document_detail/2510234.html" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">\u300A\u57FA\u4E8E\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u4E0E\u7075\u79EF\u5B9E\u73B0\u8BED\u4E49\u641C\u7D22\u300B</a>\u3002</p>
+<p>\u4EE5\u4E0B\u4E3A\u4F7F\u7528RAG\u8FDB\u884C\u589E\u5F3A\u7684\u4F8B\u5B50\uFF0C\u539F\u59CB\u8BF7\u6C42\u4E3A\uFF1A</p>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#e1e4e8">\u6D77\u5357\u8FFD\u5C3E\u4E8B\u6545\uFF0C\u53D1\u751F\u5728\u54EA\u91CC\uFF1F\u539F\u56E0\u662F\u4EC0\u4E48\uFF1F\u4EBA\u5458\u4F24\u4EA1\u60C5\u51B5\u5982\u4F55\uFF1F</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="\u6D77\u5357\u8FFD\u5C3E\u4E8B\u6545\uFF0C\u53D1\u751F\u5728\u54EA\u91CC\uFF1F\u539F\u56E0\u662F\u4EC0\u4E48\uFF1F\u4EBA\u5458\u4F24\u4EA1\u60C5\u51B5\u5982\u4F55\uFF1F"><div></div></button></div></figure></div>
+<p>\u672A\u7ECF\u8FC7RAG\u63D2\u4EF6\u5904\u7406LLM\u8FD4\u56DE\u7684\u7ED3\u679C\u4E3A\uFF1A</p>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#e1e4e8">\u62B1\u6B49\uFF0C\u4F5C\u4E3AAI\u6A21\u578B\uFF0C\u6211\u65E0\u6CD5\u5B9E\u65F6\u83B7\u53D6\u548C\u66F4\u65B0\u65B0\u95FB\u4E8B\u4EF6\u7684\u5177\u4F53\u4FE1\u606F\uFF0C\u5305\u62EC\u5730\u70B9\u3001\u539F\u56E0\u3001\u4EBA\u5458\u4F24\u4EA1\u7B49\u7EC6\u8282\u3002\u5BF9\u4E8E\u6B64\u7C7B\u5177\u4F53\u4E8B\u4EF6\uFF0C\u5EFA\u8BAE\u60A8\u67E5\u9605\u6700\u65B0\u7684\u65B0\u95FB\u62A5\u9053\u6216\u5B98\u65B9\u901A\u62A5\u4EE5\u83B7\u53D6\u51C6\u786E\u4FE1\u606F\u3002\u60A8\u53EF\u4EE5\u8BBF\u95EE\u4E3B\u6D41\u5A92\u4F53\u7F51\u7AD9\u3001\u4F7F\u7528\u65B0\u95FB\u5E94\u7528\u6216\u8005\u5173\u6CE8\u76F8\u5173\u653F\u5E9C\u90E8\u95E8\u7684\u516C\u544A\u6765\u83B7\u53D6\u8FD9\u7C7B\u52A8\u6001\u8D44\u8BAF\u3002</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="\u62B1\u6B49\uFF0C\u4F5C\u4E3AAI\u6A21\u578B\uFF0C\u6211\u65E0\u6CD5\u5B9E\u65F6\u83B7\u53D6\u548C\u66F4\u65B0\u65B0\u95FB\u4E8B\u4EF6\u7684\u5177\u4F53\u4FE1\u606F\uFF0C\u5305\u62EC\u5730\u70B9\u3001\u539F\u56E0\u3001\u4EBA\u5458\u4F24\u4EA1\u7B49\u7EC6\u8282\u3002\u5BF9\u4E8E\u6B64\u7C7B\u5177\u4F53\u4E8B\u4EF6\uFF0C\u5EFA\u8BAE\u60A8\u67E5\u9605\u6700\u65B0\u7684\u65B0\u95FB\u62A5\u9053\u6216\u5B98\u65B9\u901A\u62A5\u4EE5\u83B7\u53D6\u51C6\u786E\u4FE1\u606F\u3002\u60A8\u53EF\u4EE5\u8BBF\u95EE\u4E3B\u6D41\u5A92\u4F53\u7F51\u7AD9\u3001\u4F7F\u7528\u65B0\u95FB\u5E94\u7528\u6216\u8005\u5173\u6CE8\u76F8\u5173\u653F\u5E9C\u90E8\u95E8\u7684\u516C\u544A\u6765\u83B7\u53D6\u8FD9\u7C7B\u52A8\u6001\u8D44\u8BAF\u3002"><div></div></button></div></figure></div>
+<p>\u7ECF\u8FC7RAG\u63D2\u4EF6\u5904\u7406\u540ELLM\u8FD4\u56DE\u7684\u7ED3\u679C\u4E3A\uFF1A</p>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#e1e4e8">\u6D77\u5357\u8FFD\u5C3E\u4E8B\u6545\u53D1\u751F\u5728\u6D77\u6587\u9AD8\u901F\u516C\u8DEF\u6587\u660C\u81F3\u6D77\u53E3\u65B9\u541137\u516C\u91CC\u5904\u3002\u5173\u4E8E\u4E8B\u6545\u7684\u5177\u4F53\u539F\u56E0\uFF0C\u4EA4\u8B66\u90E8\u95E8\u5F53\u65F6\u4ECD\u5728\u8FDB\u4E00\u6B65\u8C03\u67E5\u4E2D\uFF0C\u6240\u4EE5\u6839\u636E\u63D0\u4F9B\u7684\u4FE1\u606F\u65E0\u6CD5\u786E\u5B9A\u4E8B\u6545\u7684\u786E\u5207\u539F\u56E0\u3002\u4EBA\u5458\u4F24\u4EA1\u60C5\u51B5\u662F1\u4EBA\u6B7B\u4EA1\uFF08\u53F8\u673A\u5F53\u573A\u6B7B\u4EA1\uFF09\uFF0C\u53E6\u67098\u4EBA\u53D7\u4F24\uFF08\u5305\u62EC2\u540D\u513F\u7AE5\u548C6\u540D\u6210\u4EBA\uFF09\uFF0C\u6240\u6709\u53D7\u4F24\u4EBA\u5458\u90FD\u88AB\u89E3\u6551\u5E76\u9001\u5F80\u533B\u9662\u8FDB\u884C\u6CBB\u7597\u3002</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="\u6D77\u5357\u8FFD\u5C3E\u4E8B\u6545\u53D1\u751F\u5728\u6D77\u6587\u9AD8\u901F\u516C\u8DEF\u6587\u660C\u81F3\u6D77\u53E3\u65B9\u541137\u516C\u91CC\u5904\u3002\u5173\u4E8E\u4E8B\u6545\u7684\u5177\u4F53\u539F\u56E0\uFF0C\u4EA4\u8B66\u90E8\u95E8\u5F53\u65F6\u4ECD\u5728\u8FDB\u4E00\u6B65\u8C03\u67E5\u4E2D\uFF0C\u6240\u4EE5\u6839\u636E\u63D0\u4F9B\u7684\u4FE1\u606F\u65E0\u6CD5\u786E\u5B9A\u4E8B\u6545\u7684\u786E\u5207\u539F\u56E0\u3002\u4EBA\u5458\u4F24\u4EA1\u60C5\u51B5\u662F1\u4EBA\u6B7B\u4EA1\uFF08\u53F8\u673A\u5F53\u573A\u6B7B\u4EA1\uFF09\uFF0C\u53E6\u67098\u4EBA\u53D7\u4F24\uFF08\u5305\u62EC2\u540D\u513F\u7AE5\u548C6\u540D\u6210\u4EBA\uFF09\uFF0C\u6240\u6709\u53D7\u4F24\u4EBA\u5458\u90FD\u88AB\u89E3\u6551\u5E76\u9001\u5F80\u533B\u9662\u8FDB\u884C\u6CBB\u7597\u3002"><div></div></button></div></figure></div>`,d={title:"AI RAG",keywords:["AI\u7F51\u5173","AI RAG"],description:"AI RAG \u63D2\u4EF6\u914D\u7F6E\u53C2\u8003"},e="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/zh-cn/plugins/ai/others/ai-rag.md",a=void 0,o=function(){return`
+## \u529F\u80FD\u8BF4\u660E
+
+\u901A\u8FC7\u5BF9\u63A5\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u5B9E\u73B0LLM-RAG\uFF0C\u6D41\u7A0B\u5982\u56FE\u6240\u793A\uFF1A
+
+<img src="https://img.alicdn.com/imgextra/i1/O1CN01LuRVs41KhoeuzakeF_!!6000000001196-0-tps-1926-1316.jpg" width=600>
+
+## \u8FD0\u884C\u5C5E\u6027
+
+\u63D2\u4EF6\u6267\u884C\u9636\u6BB5\uFF1A\`\u9ED8\u8BA4\u9636\u6BB5\`
+\u63D2\u4EF6\u6267\u884C\u4F18\u5148\u7EA7\uFF1A\`400\`
+
+
+## \u914D\u7F6E\u8BF4\u660E
+| \u540D\u79F0             | \u6570\u636E\u7C7B\u578B            | \u586B\u5199\u8981\u6C42 | \u9ED8\u8BA4\u503C | \u63CF\u8FF0                                                                               |
+|----------------|-----------------|------|-----|----------------------------------------------------------------------------------|
+| \`dashscope.apiKey\` | string | \u5FC5\u586B | - | \u7528\u4E8E\u5728\u8BBF\u95EE\u901A\u4E49\u5343\u95EE\u670D\u52A1\u65F6\u8FDB\u884C\u8BA4\u8BC1\u7684\u4EE4\u724C\u3002 |
+| \`dashscope.serviceFQDN\` | string | \u5FC5\u586B | - | \u901A\u4E49\u5343\u95EE\u670D\u52A1\u540D |
+| \`dashscope.servicePort\` | int | \u5FC5\u586B | - | \u901A\u4E49\u5343\u95EE\u670D\u52A1\u7AEF\u53E3 |
+| \`dashscope.serviceHost\` | string | \u5FC5\u586B | - | \u8BBF\u95EE\u901A\u4E49\u5343\u95EE\u670D\u52A1\u65F6\u57DF\u540D |
+| \`dashvector.apiKey\` | string | \u5FC5\u586B | - | \u7528\u4E8E\u5728\u8BBF\u95EE\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u65F6\u8FDB\u884C\u8BA4\u8BC1\u7684\u4EE4\u724C\u3002 |
+| \`dashvector.serviceFQDN\` | string | \u5FC5\u586B | - | \u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u540D |
+| \`dashvector.servicePort\` | int | \u5FC5\u586B | - | \u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u7AEF\u53E3 |
+| \`dashvector.serviceHost\` | string | \u5FC5\u586B | - | \u8BBF\u95EE\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u65F6\u57DF\u540D |
+| \`dashvector.topk\` | int | \u5FC5\u586B | - | \u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u65F6\u83B7\u53D6\u5411\u91CF\u6570 |
+| \`dashvector.threshold\` | float | \u5FC5\u586B | - | \u5411\u91CF\u8DDD\u79BB\u9608\u503C\uFF0C\u9AD8\u4E8E\u8BE5\u9608\u503C\u7684\u6587\u6863\u4F1A\u88AB\u8FC7\u6EE4\u6389 |
+| \`dashvector.field\` | string | \u5FC5\u586B | - | \u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u5B58\u50A8\u6587\u6863\u7684\u5B57\u6BB5\u540D |
+
+\u63D2\u4EF6\u5F00\u542F\u540E\uFF0C\u5728\u4F7F\u7528\u94FE\u8DEF\u8FFD\u8E2A\u529F\u80FD\u65F6\uFF0C\u4F1A\u5728span\u7684attribute\u4E2D\u6DFB\u52A0rag\u68C0\u7D22\u5230\u7684\u6587\u6863id\u4FE1\u606F\uFF0C\u4F9B\u6392\u67E5\u95EE\u9898\u4F7F\u7528\u3002
+
+## \u793A\u4F8B
+
+\`\`\`yaml
+dashscope:
+    apiKey: xxxxxxxxxxxxxxx
+    serviceFQDN: dashscope
+    servicePort: 443
+    serviceHost: dashscope.aliyuncs.com
+dashvector:
+    apiKey: xxxxxxxxxxxxxxxxxxxx
+    serviceFQDN: dashvector
+    servicePort: 443
+    serviceHost: vrs-cn-xxxxxxxxxxxxxxx.dashvector.cn-hangzhou.aliyuncs.com
+    collection: xxxxxxxxxxxxxxx
+    topk: 1
+    threshold: 0.4
+    field: raw
+\`\`\`
+
+[CEC-Corpus](https://github.com/shijiebei2009/CEC-Corpus) \u6570\u636E\u96C6\u5305\u542B 332 \u7BC7\u7A81\u53D1\u4E8B\u4EF6\u7684\u65B0\u95FB\u62A5\u9053\u7684\u8BED\u6599\u548C\u6807\u6CE8\u6570\u636E\uFF0C\u63D0\u53D6\u5176\u539F\u59CB\u7684\u65B0\u95FB\u7A3F\u6587\u672C\uFF0C\u5C06\u5176\u5411\u91CF\u5316\u540E\u6DFB\u52A0\u5230\u963F\u91CC\u4E91\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u3002\u6587\u672C\u5411\u91CF\u5316\u7684\u6559\u7A0B\u53EF\u4EE5\u53C2\u8003[\u300A\u57FA\u4E8E\u5411\u91CF\u68C0\u7D22\u670D\u52A1\u4E0E\u7075\u79EF\u5B9E\u73B0\u8BED\u4E49\u641C\u7D22\u300B](https://help.aliyun.com/document_detail/2510234.html)\u3002
+
+\u4EE5\u4E0B\u4E3A\u4F7F\u7528RAG\u8FDB\u884C\u589E\u5F3A\u7684\u4F8B\u5B50\uFF0C\u539F\u59CB\u8BF7\u6C42\u4E3A\uFF1A
+\`\`\`
+\u6D77\u5357\u8FFD\u5C3E\u4E8B\u6545\uFF0C\u53D1\u751F\u5728\u54EA\u91CC\uFF1F\u539F\u56E0\u662F\u4EC0\u4E48\uFF1F\u4EBA\u5458\u4F24\u4EA1\u60C5\u51B5\u5982\u4F55\uFF1F
+\`\`\`
+
+\u672A\u7ECF\u8FC7RAG\u63D2\u4EF6\u5904\u7406LLM\u8FD4\u56DE\u7684\u7ED3\u679C\u4E3A\uFF1A
+\`\`\`
+\u62B1\u6B49\uFF0C\u4F5C\u4E3AAI\u6A21\u578B\uFF0C\u6211\u65E0\u6CD5\u5B9E\u65F6\u83B7\u53D6\u548C\u66F4\u65B0\u65B0\u95FB\u4E8B\u4EF6\u7684\u5177\u4F53\u4FE1\u606F\uFF0C\u5305\u62EC\u5730\u70B9\u3001\u539F\u56E0\u3001\u4EBA\u5458\u4F24\u4EA1\u7B49\u7EC6\u8282\u3002\u5BF9\u4E8E\u6B64\u7C7B\u5177\u4F53\u4E8B\u4EF6\uFF0C\u5EFA\u8BAE\u60A8\u67E5\u9605\u6700\u65B0\u7684\u65B0\u95FB\u62A5\u9053\u6216\u5B98\u65B9\u901A\u62A5\u4EE5\u83B7\u53D6\u51C6\u786E\u4FE1\u606F\u3002\u60A8\u53EF\u4EE5\u8BBF\u95EE\u4E3B\u6D41\u5A92\u4F53\u7F51\u7AD9\u3001\u4F7F\u7528\u65B0\u95FB\u5E94\u7528\u6216\u8005\u5173\u6CE8\u76F8\u5173\u653F\u5E9C\u90E8\u95E8\u7684\u516C\u544A\u6765\u83B7\u53D6\u8FD9\u7C7B\u52A8\u6001\u8D44\u8BAF\u3002
+\`\`\`
+
+\u7ECF\u8FC7RAG\u63D2\u4EF6\u5904\u7406\u540ELLM\u8FD4\u56DE\u7684\u7ED3\u679C\u4E3A\uFF1A
+\`\`\`
+\u6D77\u5357\u8FFD\u5C3E\u4E8B\u6545\u53D1\u751F\u5728\u6D77\u6587\u9AD8\u901F\u516C\u8DEF\u6587\u660C\u81F3\u6D77\u53E3\u65B9\u541137\u516C\u91CC\u5904\u3002\u5173\u4E8E\u4E8B\u6545\u7684\u5177\u4F53\u539F\u56E0\uFF0C\u4EA4\u8B66\u90E8\u95E8\u5F53\u65F6\u4ECD\u5728\u8FDB\u4E00\u6B65\u8C03\u67E5\u4E2D\uFF0C\u6240\u4EE5\u6839\u636E\u63D0\u4F9B\u7684\u4FE1\u606F\u65E0\u6CD5\u786E\u5B9A\u4E8B\u6545\u7684\u786E\u5207\u539F\u56E0\u3002\u4EBA\u5458\u4F24\u4EA1\u60C5\u51B5\u662F1\u4EBA\u6B7B\u4EA1\uFF08\u53F8\u673A\u5F53\u573A\u6B7B\u4EA1\uFF09\uFF0C\u53E6\u67098\u4EBA\u53D7\u4F24\uFF08\u5305\u62EC2\u540D\u513F\u7AE5\u548C6\u540D\u6210\u4EBA\uFF09\uFF0C\u6240\u6709\u53D7\u4F24\u4EBA\u5458\u90FD\u88AB\u89E3\u6551\u5E76\u9001\u5F80\u533B\u9662\u8FDB\u884C\u6CBB\u7597\u3002
+\`\`\`
+`},c=function(){return s},n=function(){return[{depth:2,slug:"\u529F\u80FD\u8BF4\u660E",text:"\u529F\u80FD\u8BF4\u660E"},{depth:2,slug:"\u8FD0\u884C\u5C5E\u6027",text:"\u8FD0\u884C\u5C5E\u6027"},{depth:2,slug:"\u914D\u7F6E\u8BF4\u660E",text:"\u914D\u7F6E\u8BF4\u660E"},{depth:2,slug:"\u793A\u4F8B",text:"\u793A\u4F8B"}]},t=r((E,g,f)=>{const{layout:m,...i}=d;return i.file=e,i.url=a,x`${l()}${v(s)}`})});export{t as Content,y as __tla,c as compiledContent,t as default,e as file,d as frontmatter,n as getHeadings,o as rawContent,a as url};
---
+++ _astro/new-contributor-guide_dev.DPIyXpid.js
@@ -0,0 +1,52 @@
+import{c as o,__tla as u}from"./astro-component.CXlaF79K.js";import{r as c,m as d,u as h,__tla as g}from"./constant.DAvxYmA1.js";import{__tla as b}from"./astro/assets-service.DQopRnBr.js";let e,r,t,a,p,n,i,E=Promise.all([(()=>{try{return u}catch{}})(),(()=>{try{return g}catch{}})(),(()=>{try{return b}catch{}})()]).then(async()=>{let s;s=`<p>\u8FD9\u7BC7\u5411\u5BFC\u65E8\u5728\u7ED9\u6B63\u5728\u51C6\u5907\u5411 Higress \u63D0\u4EA4\u8D21\u732E\u7684\u65B0\u624B\u63D0\u4F9B\u6307\u5BFC\u3002</p>
+<h3 id="\u90AE\u4EF6\u5217\u8868\u63CF\u8FF0">\u90AE\u4EF6\u5217\u8868\u63CF\u8FF0</h3>
+<p>TBD</p>
+<h3 id="\u62A5\u544A\u95EE\u9898">\u62A5\u544A\u95EE\u9898</h3>
+<p>\u60A8\u59CB\u7EC8\u53EF\u4EE5\u901A\u8FC7Github <a href="https://github.com/alibaba/higress/issues" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">Issues</a> \u5411Higress\u62A5\u544A\u95EE\u9898\u3002</p>
+<p>\u5982\u679C\u60A8\u6B63\u5728\u62A5\u544Abug\uFF0C\u8BF7\u53C2\u9605\u95EE\u9898\u62A5\u544A<a href="https://github.com/alibaba/higress/issues/new?template=BUG_REPORT.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">\u6A21\u7248</a>\u3002</p>
+<p>\u5982\u679C\u60A8\u6B63\u5728\u62A5\u544A\u529F\u80FD\u8981\u6C42\uFF0C\u8BF7\u53C2\u9605\u95EE\u9898\u62A5\u544A<a href="https://github.com/alibaba/higress/issues/new?template=FEATURE_REQUEST.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">\u6A21\u7248</a>\u3002</p>
+<p>\u5982\u679C\u60A8\u6B63\u5728\u62A5\u544A\u5E38\u89C4\u95EE\u9898\uFF0C\u6BD4\u5982\u63D0\u51FA\u4E00\u4E2A\u95EE\u9898\uFF0C\u5219\u53EF\u4EE5\u6253\u5F00<a href="https://github.com/alibaba/higress/issues/new" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">\u5E38\u89C4\u95EE\u9898</a></p>
+<h3 id="\u53D1\u9001-pull-request">\u53D1\u9001 pull request</h3>
+<ul>
+<li>\u53C2\u8003<a href="https://github.com/alibaba/higress/blob/main/.github/PULL_REQUEST_TEMPLATE.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">pull request template</a></li>
+<li>\u5728\u60A8\u53D1\u9001pull request\u4E4B\u524D\uFF0C\u8BF7\u540C\u6B65\u60A8\u7684github\u4ED3\u5E93\u548C\u8FDC\u7A0B\u4ED3\u5E93\uFF0C\u8FD9\u4F1A\u4F7F\u60A8\u7684pull request\u7B80\u5355\u660E\u4E86\uFF0C\u5177\u4F53\u64CD\u4F5C\u8BF7\u770B\u5982\u4E0B\u6240\u793A\u6B65\u9AA4\uFF1A</li>
+</ul>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">remote</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">add</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">upstream</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">git@github.com:alibaba/higress.git</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">fetch</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">upstream</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">rebase</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">upstream/main</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">checkout</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-b</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">your_awesome_patch</span></div></div><div class="ec-line"><div class="code"><span style="--0:#79B8FF">...</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">add</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">some</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">work</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">push</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">origin</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">your_awesome_patch</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="git remote add upstream git@github.com:alibaba/higress.git\x7Fgit fetch upstream\x7Fgit rebase upstream/main\x7Fgit checkout -b your_awesome_patch\x7F... add some work\x7Fgit push origin your_awesome_patch"><div></div></button></div></figure></div>
+<h3 id="\u7F16\u7801\u89C4\u8303">\u7F16\u7801\u89C4\u8303</h3>
+<p>\u8BF7\u6309\u7167<a href="https://github.com/alibaba/higress/blob/main/CONTRIBUTING_CN.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">CONTRIBUTING.md</a>\u4E2D\u7684\u7F16\u7801\u89C4\u8303\u5BF9\u81EA\u5DF1\u7684\u4EE3\u7801\u8FDB\u884C\u68C0\u67E5\u3002</p>`,a={title:"\u65B0\u8D21\u732E\u8005\u5411\u5BFC",keywords:["Higress"],description:"\u8FD9\u7BC7\u5411\u5BFC\u65E8\u5728\u7ED9\u6B63\u5728\u51C6\u5907\u5411 Higress \u63D0\u4EA4\u8D21\u732E\u7684\u65B0\u624B\u63D0\u4F9B\u6307\u5BFC\u3002",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/developers/contributor-guide/new-contributor-guide_dev.md"},t="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/zh-cn/developers/contributor-guide/new-contributor-guide_dev.md",i=void 0,n=function(){return`
+# \u65B0\u8D21\u732E\u8005\u5411\u5BFC
+
+\u8FD9\u7BC7\u5411\u5BFC\u65E8\u5728\u7ED9\u6B63\u5728\u51C6\u5907\u5411 Higress \u63D0\u4EA4\u8D21\u732E\u7684\u65B0\u624B\u63D0\u4F9B\u6307\u5BFC\u3002
+
+### \u90AE\u4EF6\u5217\u8868\u63CF\u8FF0
+
+TBD
+
+### \u62A5\u544A\u95EE\u9898
+
+\u60A8\u59CB\u7EC8\u53EF\u4EE5\u901A\u8FC7Github [Issues](https://github.com/alibaba/higress/issues) \u5411Higress\u62A5\u544A\u95EE\u9898\u3002
+
+\u5982\u679C\u60A8\u6B63\u5728\u62A5\u544Abug\uFF0C\u8BF7\u53C2\u9605\u95EE\u9898\u62A5\u544A[\u6A21\u7248](https://github.com/alibaba/higress/issues/new?template=BUG_REPORT.md)\u3002
+
+\u5982\u679C\u60A8\u6B63\u5728\u62A5\u544A\u529F\u80FD\u8981\u6C42\uFF0C\u8BF7\u53C2\u9605\u95EE\u9898\u62A5\u544A[\u6A21\u7248](https://github.com/alibaba/higress/issues/new?template=FEATURE_REQUEST.md)\u3002
+
+\u5982\u679C\u60A8\u6B63\u5728\u62A5\u544A\u5E38\u89C4\u95EE\u9898\uFF0C\u6BD4\u5982\u63D0\u51FA\u4E00\u4E2A\u95EE\u9898\uFF0C\u5219\u53EF\u4EE5\u6253\u5F00[\u5E38\u89C4\u95EE\u9898](https://github.com/alibaba/higress/issues/new)
+
+### \u53D1\u9001 pull request
+
+* \u53C2\u8003[pull request template](https://github.com/alibaba/higress/blob/main/.github/PULL_REQUEST_TEMPLATE.md)
+* \u5728\u60A8\u53D1\u9001pull request\u4E4B\u524D\uFF0C\u8BF7\u540C\u6B65\u60A8\u7684github\u4ED3\u5E93\u548C\u8FDC\u7A0B\u4ED3\u5E93\uFF0C\u8FD9\u4F1A\u4F7F\u60A8\u7684pull request\u7B80\u5355\u660E\u4E86\uFF0C\u5177\u4F53\u64CD\u4F5C\u8BF7\u770B\u5982\u4E0B\u6240\u793A\u6B65\u9AA4\uFF1A
+
+\`\`\`sh
+git remote add upstream git@github.com:alibaba/higress.git
+git fetch upstream
+git rebase upstream/main
+git checkout -b your_awesome_patch
+... add some work
+git push origin your_awesome_patch
+\`\`\`
+
+### \u7F16\u7801\u89C4\u8303
+
+\u8BF7\u6309\u7167[CONTRIBUTING.md](https://github.com/alibaba/higress/blob/main/CONTRIBUTING_CN.md)\u4E2D\u7684\u7F16\u7801\u89C4\u8303\u5BF9\u81EA\u5DF1\u7684\u4EE3\u7801\u8FDB\u884C\u68C0\u67E5\u3002
+`},r=function(){return s},p=function(){return[{depth:3,slug:"\u90AE\u4EF6\u5217\u8868\u63CF\u8FF0",text:"\u90AE\u4EF6\u5217\u8868\u63CF\u8FF0"},{depth:3,slug:"\u62A5\u544A\u95EE\u9898",text:"\u62A5\u544A\u95EE\u9898"},{depth:3,slug:"\u53D1\u9001-pull-request",text:"\u53D1\u9001 pull request"},{depth:3,slug:"\u7F16\u7801\u89C4\u8303",text:"\u7F16\u7801\u89C4\u8303"}]},e=o((m,y,_)=>{const{layout:f,...l}=a;return l.file=t,l.url=i,c`${d()}${h(s)}`})});export{e as Content,E as __tla,r as compiledContent,e as default,t as file,a as frontmatter,p as getHeadings,n as rawContent,i as url};
