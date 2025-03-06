a1c4fffed405ad836aa9d3b10b9f11697b99d709
johnlanni <johnlanni@users.noreply.github.com>
2025-03-06 12:51:26 +0000

    Automated deployment: Thu Mar  6 12:51:26 UTC 2025 d9ad45a31a93c2698b3be5f08d18d9c1f6a35c22

---
+++ _astro/new-contributor-guide_dev.YdY4dCQr.js
@@ -0,0 +1,52 @@
+import{c as p,__tla as u}from"./astro-component.CXlaF79K.js";import{r as c,m as g,u as h,__tla as d}from"./constant.DXPxw1OC.js";import{__tla as b}from"./astro/assets-service.DQopRnBr.js";let s,n,t,a,o,l,i,m=Promise.all([(()=>{try{return u}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return b}catch{}})()]).then(async()=>{let e;e=`<p>This is a guide for new contributor who wants to contribute to Higress.</p>
+<h3 id="subscribe-to-the-mailing-list">Subscribe to the mailing list</h3>
+<p>TBD</p>
+<h3 id="reporting-issue">Reporting issue</h3>
+<p>You can always reporting an issue to Higress via Github <a href="https://github.com/alibaba/higress/issues" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">Issues</a>.</p>
+<p>If you are reporting bugs, please refer to the issue report <a href="https://github.com/alibaba/higress/issues/new?template=BUG_REPORT.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">template</a>.</p>
+<p>If you are reporting feature, please refer to the issue report <a href="https://github.com/alibaba/higress/issues/new?template=FEATURE_REQUEST.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">template</a>.</p>
+<p>If you are reporting regular issues, like raise an question, you can open an <a href="https://github.com/alibaba/higress/issues/new" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">regular issue</a>.</p>
+<h3 id="sending-pull-request">Sending pull request</h3>
+<ul>
+<li>Follow the checklist in the <a href="https://github.com/alibaba/higress/blob/main/.github/PULL_REQUEST_TEMPLATE.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">pull request template</a></li>
+<li>Before you sending out the pull request, please sync your forked repository with remote repository, this will make your pull request simple and clear. See guide below:</li>
+</ul>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">remote</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">add</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">upstream</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">git@github.com:alibaba/higress.git</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">fetch</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">upstream</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">rebase</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">upstream/main</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">checkout</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-b</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">your_awesome_patch</span></div></div><div class="ec-line"><div class="code"><span style="--0:#79B8FF">...</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">add</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">some</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">work</span></div></div><div class="ec-line"><div class="code"><span style="--0:#B392F0">git</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">push</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">origin</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">your_awesome_patch</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="git remote add upstream git@github.com:alibaba/higress.git\x7Fgit fetch upstream\x7Fgit rebase upstream/main\x7Fgit checkout -b your_awesome_patch\x7F... add some work\x7Fgit push origin your_awesome_patch"><div></div></button></div></figure></div>
+<h3 id="code-convention">Code convention</h3>
+<p>Please check the <a href="https://github.com/alibaba/higress/blob/main/CONTRIBUTING_EN.md" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">CONTRIBUTING.md</a> for code convention.</p>`,a={title:"New contributor guide",keywords:["Higress","contributor"],description:"This is a guide for new comers who wants to contribute to Higress.",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/developers/contributor-guide/new-contributor-guide_dev.md"},t="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/developers/en/contributor-guide/new-contributor-guide_dev.md",i=void 0,l=function(){return`
+# New contributor guide
+
+This is a guide for new contributor who wants to contribute to Higress.
+
+### Subscribe to the mailing list
+
+TBD
+
+### Reporting issue
+
+You can always reporting an issue to Higress via Github [Issues](https://github.com/alibaba/higress/issues).
+
+If you are reporting bugs, please refer to the issue report [template](https://github.com/alibaba/higress/issues/new?template=BUG_REPORT.md).
+
+If you are reporting feature, please refer to the issue report [template](https://github.com/alibaba/higress/issues/new?template=FEATURE_REQUEST.md).
+
+If you are reporting regular issues, like raise an question, you can open an [regular issue](https://github.com/alibaba/higress/issues/new).
+
+### Sending pull request
+
+* Follow the checklist in the [pull request template](https://github.com/alibaba/higress/blob/main/.github/PULL_REQUEST_TEMPLATE.md)
+* Before you sending out the pull request, please sync your forked repository with remote repository, this will make your pull request simple and clear. See guide below:
+
+\`\`\`sh
+git remote add upstream git@github.com:alibaba/higress.git
+git fetch upstream
+git rebase upstream/main
+git checkout -b your_awesome_patch
+... add some work
+git push origin your_awesome_patch
+\`\`\`
+
+### Code convention
+
+Please check the [CONTRIBUTING.md](https://github.com/alibaba/higress/blob/main/CONTRIBUTING_EN.md) for code convention.
+`},n=function(){return e},o=function(){return[{depth:3,slug:"subscribe-to-the-mailing-list",text:"Subscribe to the mailing list"},{depth:3,slug:"reporting-issue",text:"Reporting issue"},{depth:3,slug:"sending-pull-request",text:"Sending pull request"},{depth:3,slug:"code-convention",text:"Code convention"}]},s=p((E,y,f)=>{const{layout:_,...r}=a;return r.file=t,r.url=i,c`${g()}${h(e)}`})});export{s as Content,m as __tla,n as compiledContent,s as default,t as file,a as frontmatter,o as getHeadings,l as rawContent,i as url};
