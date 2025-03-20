f2e2fc94a842ce9105d0beddf703bcfdf3b3ae29
CH3CHO <CH3CHO@users.noreply.github.com>
2025-03-20 05:51:21 +0000

    Automated deployment: Thu Mar 20 05:51:20 UTC 2025 439da742b646f2b7b7043bb1b8f5cd1fea2f2313

---
+++ _astro/multi-proxy.prmn5OqD.js
@@ -0,0 +1,109 @@
+import{c as p,__tla as c}from"./astro-component.CXlaF79K.js";import{r as d,m as u,u as h,__tla as g}from"./constant.CHkMGfMv.js";import{__tla as m}from"./astro/assets-service.DQopRnBr.js";let s,i,t,n,r,l,o,f=Promise.all([(()=>{try{return c}catch{}})(),(()=>{try{return g}catch{}})(),(()=>{try{return m}catch{}})()]).then(async()=>{let e;e=`<h1 id="scene-description">Scene Description</h1>
+<p>The AI gateway can forward external calls to different large models through a unified invocation method to the corresponding large models internally, making backend model scheduling more flexible. Higress AI Gateway supports the unified protocol conversion of over 100 commonly used models and also supports model-level fallback.</p>
+<p>During the evaluation of large models, the multi-model proxy function can construct a unified dataset, forwarding model requests to backend models to verify the effectiveness of the models. Combined with observability plugins, it can clearly track the chains of different models.</p>
+<h1 id="deploy-higress-ai-gateway">Deploy Higress AI Gateway</h1>
+<p>This guide is based on Docker deployment. If you need other deployment methods (such as k8s, helm, etc.), please refer to <a href="https://higress.cn/docs/latest/user/quickstart/" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">Quick Start</a>\u3002</p>
+<p>Execute the following command:</p>
+<div class="expressive-code"><link rel="stylesheet" href="/_astro/ec.1vui4.css"><script type="module" src="/_astro/ec.dy9ns.js"><\/script><figure class="frame is-terminal not-content"><figcaption class="header"><span class="title"></span><span class="sr-only">Terminal window</span></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#B392F0">curl</span><span style="--0:#E1E4E8"> </span><span style="--0:#79B8FF">-sS</span><span style="--0:#E1E4E8"> </span><span style="--0:#9ECBFF">https://higress.cn/ai-gateway/install.sh</span><span style="--0:#E1E4E8"> </span><span style="--0:#F97583">|</span><span style="--0:#E1E4E8"> </span><span style="--0:#B392F0">bash</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="curl -sS https://higress.cn/ai-gateway/install.sh | bash"><div></div></button></div></figure></div>
+<p>Follow the prompts to enter the Aliyun Dashscope or other API-KEY; you can also press Enter to skip and modify it later in the console. You can also press <code dir="auto">Enter</code> to skip and modify it later in the console.</p>
+<p><img src="/img/1742448716754.png" alt="" referrerpolicy="no-referrer"></p>
+<p>The default HTTP service port is 8080, the HTTPS service port is 8443, and the console service port is 8001. If you need to use other ports, download the deployment script using <code dir="auto">wget https://higress.cn/ai-gateway/install.sh</code>, modify DEFAULT_GATEWAY_HTTP_PORT/DEFAULT_GATEWAY_HTTPS_PORT/DEFAULT_CONSOLE_PORT, and then execute the script using bash.</p>
+<p><img src="/img/1742448720051.png" alt="" referrerpolicy="no-referrer"></p>
+<p>After the deployment is completed, the following command display will appear.</p>
+<p><img src="/img/1742448723581.png" alt="" referrerpolicy="no-referrer"></p>
+<h1 id="console-configuration">Console Configuration</h1>
+<p>Access the Higress console via a browser at <a href="http://localhost:8001/" referrerpolicy="unsafe-url" rel="nofollow" target="_blank">http://localhost:8001/</a>. The first login requires setting up an administrator account and password.</p>
+<p>In the <code dir="auto">LLM Provider Management</code>, you can configure the API-KEYs for integrated suppliers. Currently integrated suppliers include Alibaba Cloud, DeepSeek, Azure OpenAI, OpenAI, DouBao, etc. Here we configure multi-model proxies for Tongyi Qwen and DeepSeek.</p>
+<p><img src="/img/1742448727611.png" alt="" referrerpolicy="no-referrer"></p>
+<p>In the <code dir="auto">AI Route Config</code>, configure the fallback settings for the DeepSeek route. When the request to the target service fails (e.g., due to rate limiting or access failure), it will fallback to the Alibaba Cloud qwen-turbo model.</p>
+<p><img src="/img/1742448732555.png" alt="" referrerpolicy="no-referrer"></p>
+<p><img src="/img/1742448737606.png" alt="" referrerpolicy="no-referrer"></p>
+<h1 id="debugging">Debugging</h1>
+<p>Open the system\u2019s built-in command line and send a request using the following command (if the HTTP service is not deployed on port 8080, modify it to the corresponding port):</p>
+<div class="expressive-code"><figure class="frame not-content"><figcaption class="header"></figcaption><pre tabindex="0"><code><div class="ec-line"><div class="code"><span style="--0:#9ECBFF">curl 'http://localhost:8080/v1/chat/completions' \\</span></div></div><div class="ec-line"><div class="code"><span class="indent">  </span><span style="--0:#85E89D">-H 'Content-Type</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">application/json' \\</span></div></div><div class="ec-line"><div class="code"><span class="indent">  </span><span style="--0:#9ECBFF">-d '{</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#9ECBFF">"model"</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">"qwen-max"</span><span style="--0:#E1E4E8">,</span></div></div><div class="ec-line"><div class="code"><span class="indent">    </span><span style="--0:#9ECBFF">"messages"</span><span style="--0:#E1E4E8">: [</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">      </span></span><span style="--0:#E1E4E8">{</span></div></div><div class="ec-line"><div class="code"><span class="indent">        </span><span style="--0:#9ECBFF">"role"</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">"user"</span><span style="--0:#E1E4E8">,</span></div></div><div class="ec-line"><div class="code"><span class="indent">        </span><span style="--0:#9ECBFF">"content"</span><span style="--0:#E1E4E8">: </span><span style="--0:#9ECBFF">"Who are you?"</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">      </span></span><span style="--0:#E1E4E8">}</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">    </span></span><span style="--0:#E1E4E8">]</span></div></div><div class="ec-line"><div class="code"><span class="indent"><span style="--0:#E1E4E8">  </span></span><span style="--0:#E1E4E8">}</span><span style="--0:#9ECBFF">'</span></div></div></code></pre><div class="copy"><button title="Copy to clipboard" data-copied="Copied!" data-code="curl &#x27;http://localhost:8080/v1/chat/completions&#x27; \\\x7F  -H &#x27;Content-Type: application/json&#x27; \\\x7F  -d &#x27;{\x7F    &#x22;model&#x22;: &#x22;qwen-max&#x22;,\x7F    &#x22;messages&#x22;: [\x7F      {\x7F        &#x22;role&#x22;: &#x22;user&#x22;,\x7F        &#x22;content&#x22;: &#x22;Who are you?&#x22;\x7F      }\x7F    ]\x7F  }&#x27;"><div></div></button></div></figure></div>
+<p>Sample response:</p>
+<p><img src="/img/1742448741035.png" alt="" referrerpolicy="no-referrer"></p>
+<h1 id="observability">Observability</h1>
+<p>In the <code dir="auto">AI Dashboard</code>, you can observe AI requests. Observability metrics include the number of input/output tokens per second, token usage by each provider/model, etc.</p>
+<p><img src="/img/1742448745042.png" alt="" referrerpolicy="no-referrer"></p>`,n={title:"Multi-Model Proxy",description:"Open-source workflow for multi-model proxy",date:"2025-03-03",category:"article",keywords:["Higress"],authors:"\u5B50\u91C9"},t="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/ai/en/scene-guide/multi-proxy.md",o=void 0,l=function(){return`# Scene Description
+The AI gateway can forward external calls to different large models through a unified invocation method to the corresponding large models internally, making backend model scheduling more flexible. Higress AI Gateway supports the unified protocol conversion of over 100 commonly used models and also supports model-level fallback.
+
+During the evaluation of large models, the multi-model proxy function can construct a unified dataset, forwarding model requests to backend models to verify the effectiveness of the models. Combined with observability plugins, it can clearly track the chains of different models.
+
+# Deploy Higress AI Gateway
+This guide is based on Docker deployment. If you need other deployment methods (such as k8s, helm, etc.), please refer to [Quick Start](https://higress.cn/docs/latest/user/quickstart/)\u3002
+
+
+
+Execute the following command:
+
+\`\`\`bash
+curl -sS https://higress.cn/ai-gateway/install.sh | bash
+\`\`\`
+
+Follow the prompts to enter the Aliyun Dashscope or other API-KEY; you can also press Enter to skip and modify it later in the console. You can also press \`Enter\` to skip and modify it later in the console.
+
+![](/img/1742448716754.png)
+
+
+
+The default HTTP service port is 8080, the HTTPS service port is 8443, and the console service port is 8001. If you need to use other ports, download the deployment script using \`wget https://higress.cn/ai-gateway/install.sh\`, modify DEFAULT_GATEWAY_HTTP_PORT/DEFAULT_GATEWAY_HTTPS_PORT/DEFAULT_CONSOLE_PORT, and then execute the script using bash.
+
+![](/img/1742448720051.png)
+
+
+
+After the deployment is completed, the following command display will appear.
+
+![](/img/1742448723581.png)
+
+
+
+# Console Configuration
+Access the Higress console via a browser at [http://localhost:8001/](http://localhost:8001/). The first login requires setting up an administrator account and password.
+
+In the \`LLM Provider Management\`, you can configure the API-KEYs for integrated suppliers. Currently integrated suppliers include Alibaba Cloud, DeepSeek, Azure OpenAI, OpenAI, DouBao, etc. Here we configure multi-model proxies for Tongyi Qwen and DeepSeek.
+
+![](/img/1742448727611.png)
+
+
+
+
+In the \`AI Route Config\`, configure the fallback settings for the DeepSeek route. When the request to the target service fails (e.g., due to rate limiting or access failure), it will fallback to the Alibaba Cloud qwen-turbo model.
+
+![](/img/1742448732555.png)
+
+![](/img/1742448737606.png)
+
+
+
+# Debugging
+Open the system's built-in command line and send a request using the following command (if the HTTP service is not deployed on port 8080, modify it to the corresponding port):
+
+\`\`\`yaml
+curl 'http://localhost:8080/v1/chat/completions' \\
+  -H 'Content-Type: application/json' \\
+  -d '{
+    "model": "qwen-max",
+    "messages": [
+      {
+        "role": "user",
+        "content": "Who are you?"
+      }
+    ]
+  }'
+
+\`\`\`
+
+Sample response:
+
+![](/img/1742448741035.png)
+
+
+
+# Observability
+In the \`AI Dashboard\`, you can observe AI requests. Observability metrics include the number of input/output tokens per second, token usage by each provider/model, etc.
+
+![](/img/1742448745042.png)
+
+`},i=function(){return e},r=function(){return[{depth:1,slug:"scene-description",text:"Scene Description"},{depth:1,slug:"deploy-higress-ai-gateway",text:"Deploy Higress AI Gateway"},{depth:1,slug:"console-configuration",text:"Console Configuration"},{depth:1,slug:"debugging",text:"Debugging"},{depth:1,slug:"observability",text:"Observability"}]},s=p((y,v,E)=>{const{layout:b,...a}=n;return a.file=t,a.url=o,d`${u()}${h(e)}`})});export{s as Content,f as __tla,i as compiledContent,s as default,t as file,n as frontmatter,r as getHeadings,l as rawContent,o as url};
---
+++ _astro/token-management.CrM9_oGt.js
@@ -0,0 +1,207 @@
+const e="en/scene-guide/token-management.md",t="ai",n="en/scene-guide/token-management",i=`
+# Scene Description
+The AI gateway can track the number of tokens used by large models and impose restrictions when consumers exceed their limits, thereby better managing user quotas for AI applications and providing data support for token usage analysis.
+
+The token management scenario is based on consumer authentication, token rate limiting, and token quota plugins. It integrates observability capabilities to transform token resources into quantifiable, manageable, and optimizable service units. Based on custom strategies, it ensures the stability, security, and fairness of services under high concurrency.
+
+
+
+# Deploy Higress AI Gateway
+This guide is based on Docker deployment. If you need other deployment methods (such as k8s, helm, etc.), please refer to [Quick Start](https://higress.cn/docs/latest/user/quickstart/)\u3002
+
+
+
+Execute the following command:
+
+\`\`\`bash
+curl -sS https://higress.cn/ai-gateway/install.sh | bash
+\`\`\`
+
+Follow the prompts to enter the Aliyun Dashscope or other API-KEY; you can also press Enter to skip and modify it later in the console. You can also press \`Enter\` to skip and modify it later in the console.
+
+![](/img/1742448795176.png)
+
+
+
+The default HTTP service port is 8080, the HTTPS service port is 8443, and the console service port is 8001. If you need to use other ports, download the deployment script using \`wget https://higress.cn/ai-gateway/install.sh\`, modify DEFAULT_GATEWAY_HTTP_PORT/DEFAULT_GATEWAY_HTTPS_PORT/DEFAULT_CONSOLE_PORT, and then execute the script using bash.
+
+![](/img/1742448798788.png)
+
+
+
+After the deployment is completed, the following command display will appear.
+
+![](/img/1742448802740.png)
+
+
+
+# Console Configuration
+Access the Higress console via a browser at [http://localhost:8001/](http://localhost:8001/). The first login requires setting up an administrator account and password.
+
+In the \`LLM Provider Management\`, you can configure the API-KEYs for integrated suppliers. Currently integrated suppliers include Alibaba Cloud, DeepSeek, Azure OpenAI, OpenAI, DouBao, etc. Here we configure multi-model proxies for Tongyi Qwen, which can be ignored if already configured in the previous step.
+
+![](/img/1742448806791.png)
+
+
+
+## Configure Consumers
+In the consumer management interface of the console, add consumers for the current gateway to manage quotas and send requests.
+
+![](/img/1742448810781.png)
+
+Click to create a consumer, and based on Key Auth, create three consumers named aliyun-admin, aliyun-user1, and aliyun-user2. Authentication is performed based on the x-api-key field in the HTTP Header.
+
+![](/img/1742448813965.png)
+
+![](/img/1742448817304.png)
+
+
+
+## Configure Redis Storage Service
+Token information needs to be temporarily stored for access, so a Redis service needs to be created for caching. This example uses Docker to set up a local Redis service for Higress.
+
+
+### Build Redis Service
+1. Use the docker command to start a redis container
+
+\`\`\`plain
+docker run --name my-redis -p 6379:6379 -d redis
+\`\`\`
+
+2. Check the IP address of the my-redis service:
+    1. Use \`docker network ls\` to get the ID of the bridge network.
+![](/img/1742448819882.png)
+
+    2. Use \`docker network inspect <network-id>\` to check if the my-redis container is connected to the bridge network.
+       1. If not, connect it to the network using the \`docker network connect bridge my-redis\` command.
+    
+3. Get the IP address of the my-redis service.
+
+![](/img/1742448822805.png)
+
+
+
+### Configure Redis Service Source
+Create a service source in the console's \`Service Source\`. Fill in the corresponding fields in the \`Service Source\`:
+
++ Type: Domains
++ Service port: 443
++ Domains: Concatenate the IP address of my-redis with the service portt
++ Service protocol: HTTP
+
+![](/img/1742448825899.png)
+
+
+
+## Configure AI Route Strategy
+### Consumer Authentication Configuration
+In the \`AI Route\`, configure consumers for aliyun and click Edit.
+
+![](/img/1742448829467.png)
+
+Enable request authentication and add the consumers created earlier.
+
+![](/img/1742448832524.png)
+
+
+
+
+
+### Token Quota Configuration
+In the \`AI Route\`, configure token quota for aliyun and click Edit.
+
+![](/img/1742448836119.png)
+
+![](/img/1742448840018.png)
+
+In the \`AI Quota\`plugin configuration interface, fill in the following fields as a reference:
+
+\`\`\`yaml
+redis_key_prefix: 'chat_quota:'
+admin_consumer: aliyun-admin
+admin_path: /quota
+redis:
+  service_name: local-redis.static
+  service_port: 80
+  timeout: 2000
+\`\`\`
+
+![](/img/1742448843302.png)
+
+### Token Rate Limiting Configuration
+In the \`AI Route\`, configure token rate limit for aliyun and click Edit.
+
+![](/img/1742448847117.png)
+
+![](/img/1742448850721.png)
+
+In the \`AI Token Rate Limiting\` plugin configuration interface, fill in the following fields as a reference:
+
+\`\`\`yaml
+rule_items:
+- limit_by_per_header: x-api-key
+  limit_keys:
+  - key: "*"
+    token_per_minute: 5 # Limit to 5 tokens per minute
+rule_name: "default_rule"
+
+redis:
+  service_name: local-redis.static
+  service_port: 80
+\`\`\`
+
+![](/img/1742448854156.png)
+
+
+
+# Debugging
+Open the system's built-in command line and send a request using the following command (if the HTTP service is not deployed on port 8080, modify it to the corresponding port):
+
+\`\`\`yaml
+# Query quota, x-api-key is the credential for aliyun-admin
+curl 'http://localhost:8080/v1/chat/completions/quota?consumer=aliyun-user1' \\
+  -H 'x-api-key:xxxxxxxxxxxx' \\
+  -H 'x-higress-llm-model: qwen-max'
+  
+# Refresh quota, x-api-key is the credential for aliyun-admin
+curl 'http://localhost:8080/v1/chat/completions/quota/refresh' \\
+  -d 'consumer=aliyun-user1&quota=100' \\
+  -H 'x-api-key:xxxxxxxxxxxx' \\
+  -H 'x-higress-llm-model: qwen-max'
+  
+# Increase quota, x-api-key is the credential for aliyun-admin
+curl 'http://localhost:8080/v1/chat/completions/quota/delta' \\
+  -d 'consumer=aliyun-user1&value=100' \\
+  -H 'x-api-key:xxxxxxxxxxxx' \\
+  -H 'x-higress-llm-model: qwen-max'
+
+# Request, x-api-key is the credential for aliyun-user1
+curl 'http://localhost:8080/v1/chat/completions' \\
+  -H 'x-api-key:xxxxxxxxxxxx' \\
+  -H 'Content-Type: application/json' \\
+  -d '{
+    "model": "qwen-max",
+    "messages": [
+      {
+        "role": "user",
+        "content": "Who are you?"
+      }
+    ]
+  }'
+
+\`\`\`
+
+Sample response:
+
+![](/img/1742448859026.png)
+
+![](/img/1742448862951.png)
+
+
+
+# Observability
+In the \`AI Dashboard\`, you can observe AI requests. Observability metrics include the number of input/output tokens per second, token usage by each provider/model, and the token usage of consumers etc.
+
+![](/img/1742448866900.png)
+
+`,o={title:"Token Management",description:"Open-source workflow for token management",keywords:["Higress"],date:"2025-03-03",category:"article"},s={type:"content",filePath:"/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/ai/en/scene-guide/token-management.md",rawData:void 0};export{s as _internal,i as body,t as collection,o as data,e as id,n as slug};
