ad9637878bda5377d4cba50d31af63a80956a74d
Hazel0928 <Hazel0928@users.noreply.github.com>
2025-04-14 10:41:31 +0000

    deploy: 90816d7f29fa395a869f6fdb4a41179d7950eb31

---
+++ _astro/configurations.DErHhr6i.js
@@ -0,0 +1,5 @@
+import{c,__tla as l}from"./astro-component.m4CCZO6J.js";import{r as g,m,u as _,__tla as d}from"./constant.n-J9LWvA.js";import{__tla as h}from"./astro/assets-service.BhtN0nM5.js";let r,i,e,s,n,u,a,p=Promise.all([(()=>{try{return l}catch{}})(),(()=>{try{return d}catch{}})(),(()=>{try{return h}catch{}})()]).then(async()=>{let t;t="<p>TBD</p>",s={title:"Higress Parameters Guide",keywords:["Higress"],description:"Higress Parameters Guide\u3002",custom_edit_url:"https://github.com/higress-group/higress-group.github.io/blob/main/i18n/zh-cn/docusaurus-plugin-content-docs/current/user/configurations.md"},e="/home/runner/work/higress-group.github.io/higress-group.github.io/src/content/docs/latest/en/user/configurations.md",a=void 0,u=function(){return`
+# Higress Parameters Guide
+
+TBD
+`},i=function(){return t},n=function(){return[]},r=c((f,b,y)=>{const{layout:H,...o}=s;return o.file=e,o.url=a,g`${m()}${_(t)}`})});export{r as Content,p as __tla,i as compiledContent,r as default,e as file,s as frontmatter,n as getHeadings,u as rawContent,a as url};
