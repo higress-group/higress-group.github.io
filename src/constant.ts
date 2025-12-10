e8e99aea8b163cfc8f2466cb25d4673baaa5eccd
heimanba <371510756@qq.com>
2025-12-10 17:28:43 +0800

    update By Robot2025-12-10 17:28:43

--- src/constant.ts
+++ src/constant.ts
@@ -8,7 +8,7 @@ import { getEntries } from "astro:content";
 export const TOPBAR = {
     "text": "AgentScope Java v1.0 重磅发布",
     "mobileText": "AgentScope Java v1.0 重磅发布",
-    "link": "https://mp.weixin.qq.com/s/vtb3t0JnSTau3XM6wJMEkw",
+    "link": "https://higress.cn/blog/higress-gvr7dx_awbbpb_ghgh5gf3bfnh1unp/",
     "target": "_blank",
     "display": true
 }
